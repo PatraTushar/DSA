71ec9c01ac64132d474c8ae05203ddd0eee239c7
Tushar Patra <tushar04012002@gmail.com>
2025-06-18 22:00:29 +0530

    dsa

--- src/linkedListByKK/SinglyLinkedList/InterviewQuestion/Q14.java
+++ src/linkedListByKK/SinglyLinkedList/InterviewQuestion/Q14.java
@@ -2,16 +2,17 @@ package linkedListByKK.SinglyLinkedList.InterviewQuestion;
 
 public class Q14 {
 
-    public static class Node{
+    public static class Node {
 
         int data;
         Node next;
-        Node(int data){
-            this.data=data;
+
+        Node(int data) {
+            this.data = data;
         }
     }
 
-    static void display(Node head1 , Node head2) {
+    static void display(Node head1, Node head2) {
 
         Node temp1 = head1;
         Node temp2 = head2;
@@ -24,9 +25,9 @@ public class Q14 {
         System.out.println();
 
         System.out.println(" linked list 2 ");
-        while (temp2!=null){
-            System.out.print(temp2.data +" ");
-            temp2=temp2.next;
+        while (temp2 != null) {
+            System.out.print(temp2.data + " ");
+            temp2 = temp2.next;
         }
         System.out.println();
     }
@@ -44,75 +45,74 @@ public class Q14 {
 
     }
 
-        static Node MergeTwoLL(Node head1, Node head2){
+    static Node MergeTwoLL(Node head1, Node head2) {
 
-            System.out.println(" After merging ");
+        //Time Complexity: O(N + M)
+        //Space Complexity: O(1)
 
-            Node temp1 = head1;
-            Node temp2 = head2;
-            Node dummy = new Node(100);
-            Node t = dummy;
+        System.out.println(" After merging ");
 
-
-            while (temp1 != null && temp2 != null) {
-
-                if (temp1.data < temp2.data) {
-
-                   t.next=temp1;
-                   t=temp1;
-                   temp1=temp1.next;
-
-                } else {
-
-                   t.next=temp2;
-                   t=temp2;
-                   temp2=temp2.next;
+        Node temp1 = head1;
+        Node temp2 = head2;
+        Node dummy = new Node(100);
+        Node t = dummy;
 
 
-                }
-            }
+        while (temp1 != null && temp2 != null) {
 
-            if (temp1 == null) {
-                t.next = temp2;
-            } else {
+            if (temp1.data < temp2.data) {
+
                 t.next = temp1;
+                t = temp1;
+                temp1 = temp1.next;
+
+            } else {
+
+                t.next = temp2;
+                t = temp2;
+                temp2 = temp2.next;
+
+
             }
-
-            return dummy.next;
-
         }
 
+        if (temp1 == null) {
+            t.next = temp2;
+        } else {
+            t.next = temp1;
+        }
 
+        return dummy.next;
+
+    }
 
 
     public static void main(String[] args) {
 
-      //  Merge 2 sorted linked list ( not using extra space i.e O(1))  Best approach
+        //  Merge 2 sorted linked list ( not using extra space i.e O(1))  Best approach
 
-        Node a =new Node(1);
-        Node b =new Node(3);
-        Node c =new Node(5);
-        Node d =new Node(8);
+        Node a = new Node(1);
+        Node b = new Node(3);
+        Node c = new Node(5);
+        Node d = new Node(8);
 
-        a.next=b;
-        b.next=c;
-        c.next=d;
+        a.next = b;
+        b.next = c;
+        c.next = d;
 
-        Node e =new Node(2);
-        Node f =new Node(4);
-        Node g =new Node(6);
-        Node h =new Node(7);
+        Node e = new Node(2);
+        Node f = new Node(4);
+        Node g = new Node(6);
+        Node h = new Node(7);
 
-        e.next=f;
-        f.next=g;
-        g.next=h;
+        e.next = f;
+        f.next = g;
+        g.next = h;
 
-        display(a,e);
+        display(a, e);
 
-        Node Ans=MergeTwoLL(a,e);
+        Node Ans = MergeTwoLL(a, e);
         display2(Ans);
-
-
 
 
     }
--- src/myPractice.java
+++ src/myPractice.java
@@ -1,4 +1,6 @@
 import OopsByKK.AccessModifier.Public.A;
+import linkedListByKK.SinglyLinkedList.InterviewQuestion.Q14;
+
 
 public class myPractice {
 
@@ -12,6 +14,7 @@ public class myPractice {
             this.data = data;
         }
     }
+
 
     static Node cycleII(Node head){
 
@@ -56,20 +59,89 @@ public class myPractice {
         Node g = new Node(10);
 
 
-
         a.next = b;
         b.next = c;
         c.next = d;
         d.next = e;
         e.next = f;
         f.next = g;
-        g.next=b;
+        g.next = b;
 
-        Node Ans=cycleII(a);
+        Node Ans = cycleII(a);
         System.out.println(Ans.data);
 
+        static Node merge2SortedLL (Node head1, Node head2){
 
+            Node temp1 = head1;
+            Node temp2 = head2;
+            Node dummy = new Node(-100);
+            Node t = dummy;
+
+
+            while (temp1 != null && temp2 != null) {
+
+                if (temp1.data < temp2.data) {
+
+                    t.next = temp1;
+                    t = temp1;
+                    temp1 = temp1.next;
+                } else {
+
+                    t.next = temp2;
+                    t = temp2;
+                    temp2 = temp2.next;
+                }
+
+
+            }
+
+            if (temp1 == null) {
+
+                t.next = temp2;
+
+            } else {
+                t.next = temp1;
+            }
+
+            return dummy.next;
+
+
+        }
+
+        static void display (Node head){
+
+            Node temp = head;
+
+            while (temp != null) {
+
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+
+            }
+
+        }
+
+        public static void main(String[] args){
+
+            Node a = new Node(1);
+            Node b = new Node(3);
+            Node c = new Node(5);
+            Node d = new Node(8);
+
+            a.next = b;
+            b.next = c;
+            c.next = d;
+
+            Node e = new Node(2);
+            Node f = new Node(4);
+            Node g = new Node(6);
+            Node h = new Node(7);
+
+            e.next = f;
+            f.next = g;
+            g.next = h;
+
+
+        }
 
     }
-
-}
