bea6dae9d7dec2117ce50f174fc263b11f6d1f47
Tushar Patra <tushar04012002@gmail.com>
2025-06-19 13:06:52 +0530

    Added: YourNewFile.java

---
+++ src/linkedListByKK/SinglyLinkedList/InterviewQuestion/Q31.java
@@ -0,0 +1,9 @@
+package linkedListByKK.SinglyLinkedList.InterviewQuestion;
+
+public class Q31 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" hello world ");
+    }
+}
