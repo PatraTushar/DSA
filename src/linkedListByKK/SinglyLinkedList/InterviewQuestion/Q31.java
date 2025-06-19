692431eef3be46fb7d272ad3da86320810a5bf5a
Tushar Patra <tushar04012002@gmail.com>
2025-06-19 13:15:23 +0530

    dsa

--- src/linkedListByKK/SinglyLinkedList/InterviewQuestion/Q31.java
+++
@@ -1,9 +0,0 @@
-package linkedListByKK.SinglyLinkedList.InterviewQuestion;
-
-public class Q31 {
-
-    public static void main(String[] args) {
-
-        System.out.println(" hello world ");
-    }
-}
