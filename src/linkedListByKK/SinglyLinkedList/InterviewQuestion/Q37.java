22aa75bd8b834e374ad1bd3e1983f621d9daf224
Tushar Patra <tushar04012002@gmail.com>
2025-06-24 12:37:16 +0530

    dsa

---
+++ src/linkedListByKK/SinglyLinkedList/InterviewQuestion/Q37.java
@@ -0,0 +1,93 @@
+package linkedListByKK.SinglyLinkedList.InterviewQuestion;
+
+import linkedListByKK.doublyLinkedList.Q1;
+
+public class Q37 {
+
+    public static class Node{
+
+        Node prev;
+        int data;
+        Node next;
+
+        Node(int data){
+
+            this.data=data;
+        }
+    }
+
+    static Node deleteKeys(Node head,int key){
+
+        Node temp=head;
+
+        while (temp!=null){
+
+            if(temp.data==key){
+
+                if(temp==head){
+                    head=head.next;
+                    if (head != null) head.prev = null;
+                }
+
+                Node nextNode=temp.next;
+                Node prevNode=temp.prev;
+
+                if(nextNode!=null) nextNode.prev=prevNode;
+                if(prevNode!=null) prevNode.next=nextNode;
+
+                temp=nextNode;
+            }
+
+            else {
+
+                temp=temp.next;
+            }
+        }
+
+        return head;
+
+    }
+
+    static void display(Node head){
+
+        Node temp=head;
+        while (temp!=null){
+            System.out.print(temp.data +" ");
+            temp=temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+
+        Node a=new Node(10);
+        Node b=new Node(4);
+        Node c=new Node(10);
+        Node d=new Node(10);
+        Node e=new Node(6);
+
+        a.prev=null;
+        a.next=b;
+        b.prev=a;
+        b.next=c;
+        c.prev=b;
+        c.next=d;
+        d.prev=c;
+        d.next=e;
+        e.prev=d;
+        e.next=null;
+
+        display(a);
+        int key=10;
+        Node Ans=deleteKeys(a,key);
+        display(Ans);
+
+
+
+
+
+
+
+    }
+}
