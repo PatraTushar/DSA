bdf207200d2d61a2d758518d6662484aee4eb139
Tushar Patra <tushar04012002@gmail.com>
2025-06-29 02:00:57 +0530

    dsa

--- src/myPractice.java
+++ src/myPractice.java
@@ -1,47 +1,128 @@
-//
-//
-//public class myPractice {
-//
-//
-//    static int sumOfMin(int[] arr){
-//
-//        int sum=0;
-//        int MOD = (int) 1e9 + 7;
-//
-//        for(int i=0;i<arr.length;i++){
-//
-//            int mini=arr[i];
-//
-//            for (int j=i;j<arr.length;j++){
-//
-//                mini=Math.max(mini,arr[j]);
-//                sum=(sum+mini)% MOD;
-//            }
-//
-//
-//        }
-//
-//        return sum;
-//
-//
-//
-//
-//
-//
-//    }
-//
-//
-//
-//    public static void main(String[] args) {
-//
-//        int[] arr={3,1,2,4};
-//        System.out.println(trappingRainWater(arr));
-//
-//
-//
-//
-//
-//    }
-//
-//
-//}
+import java.util.Arrays;
+import java.util.Stack;
+
+public class myPractice {
+
+
+    static int sumOfSubArrMin(int[] arr){
+
+        int n=arr.length;
+        Stack<Integer> st=new Stack<>();
+        int[] pse=new int[n];
+        int[] nse=new int[n];
+        int[] smallest=new int[n];
+
+       for(int i=0;i<n;i++){
+
+           while (!st.isEmpty() && arr[st.peek()]>=arr[i]){
+               st.pop();
+           }
+
+           pse[i]=st.isEmpty() ? -1 : st.peek();
+           st.push(i);
+       }
+
+       st.clear();
+
+       for (int i=n-1;i>=0;i--){
+
+           while (!st.isEmpty() && arr[st.peek()]>=arr[i]){
+               st.pop();
+           }
+
+           nse[i]=st.isEmpty() ? n : st.peek();
+           st.push(i);
+       }
+
+
+        long total=0;
+        int MOD = (int) 1e9 + 7;
+
+        for(int i=0;i<n;i++){
+            long left = i - pse[i];
+            long right = nse[i] - i;
+            total = (total + arr[i] * left * right) % MOD;
+
+        }
+
+
+       return (int) total;
+
+
+
+
+
+
+    }
+
+    static int sumOfSubArrMax(int[] arr){
+
+        int n=arr.length;
+        Stack<Integer> st=new Stack<>();
+        int[] pge=new int[n];
+        int[] nge=new int[n];
+
+        for(int i=0;i<n;i++){
+
+            while (!st.isEmpty() && arr[st.peek()]<=arr[i]){
+                st.pop();
+            }
+
+            pge[i]=st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+
+        st.clear();
+
+        for (int i=n-1;i>=0;i--){
+
+            while (!st.isEmpty() && arr[st.peek()]<=arr[i]){
+                st.pop();
+            }
+
+            nge[i]=st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+
+
+
+
+        long total=0;
+        int MOD = (int) 1e9 + 7;
+
+        for(int i=0;i<n;i++){
+            long left = i - pge[i];
+            long right = nge[i] - i;
+            total = (total + arr[i] * left * right) % MOD;
+
+        }
+
+        return (int) total;
+
+
+
+
+    }
+
+    static int sumOfRanges(int[] arr){
+        return sumOfSubArrMax(arr)-sumOfSubArrMin(arr);
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+        int[] arr={3,1,2,4};
+        System.out.println(sumOfSubArrMin(arr));
+
+
+
+
+
+    }
+
+
+}
