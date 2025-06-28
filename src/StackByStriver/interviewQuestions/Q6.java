c686115ae760d45234074dc7bd623aabe580bdc4
Tushar Patra <tushar04012002@gmail.com>
2025-06-28 15:27:37 +0530

    dsa

--- src/StackByStriver/interviewQuestions/Q6.java
+++ src/StackByStriver/interviewQuestions/Q6.java
@@ -38,6 +38,9 @@ public class Q6 {
 
     static int trappingRainWater(int[] height){
 
+        // Time Complexity (TC): O(N)
+        // Space Complexity (SC): O(N)
+
         int totalUnits=0;
 
         int[] leftMax=prefixMax(height);
@@ -56,10 +59,47 @@ public class Q6 {
 
     }
 
+    static int trappingRainWaterII(int[] height){
+
+        // Time Complexity (TC): O(N)
+        // Space Complexity (SC): O(1)
+
+
+
+        int n=height.length;
+        int leftMax=0;
+        int rightMax=0;
+        int totalUnits=0;
+        int l=0;
+        int r=n-1;
+
+        while (l<r){
+
+            if(height[l]<=height[r]){
+
+                if(leftMax > height[l]) totalUnits+=leftMax-height[l];
+                else leftMax=height[l];
+
+                l++;
+            }
+
+            else {
+
+                if(rightMax>height[r]) totalUnits+=rightMax-height[r];
+                else rightMax=height[r];
+
+                r--;
+            }
+        }
+
+        return totalUnits;
+    }
+
     public static void main(String[] args) {
 
         int[] arr={0,1,0,2,1,0,1,3,2,1,2,1};
         System.out.println(trappingRainWater(arr));
+        System.out.println(trappingRainWaterII(arr));
 
 
 
