e98fe5b8a07578429ba5b8c8042792dd0f58539c
Tushar Patra <tushar04012002@gmail.com>
2025-06-30 03:10:52 +0530

    dsa

--- src/StackByStriver/interviewQuestions/Q13.java
+++ src/StackByStriver/interviewQuestions/Q13.java
@@ -1,8 +1,68 @@
 package StackByStriver.interviewQuestions;
 
+import java.util.Stack;
+
 public class Q13 {
 
+    static int largestRectangleHistogram(int[] heights){
+
+        //Time Complexity (TC): O(n)
+        //Space Complexity (SC): O(n)
+
+        Stack<Integer> st=new Stack<>();
+        int n=heights.length;
+        int[] pse=new int[n];
+        int[] nse=new int[n];
+
+
+        for(int i=0;i<n;i++){
+
+            while (!st.isEmpty() && heights[st.peek()]>=heights[i]){
+                st.pop();
+            }
+
+            pse[i]=st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+
+
+        }
+
+        st.clear();
+
+
+        for(int i=n-1;i>=0;i--){
+
+            while (!st.isEmpty() && heights[st.peek()]>=heights[i]){
+                st.pop();
+            }
+
+            nse[i]=st.isEmpty() ? n : st.peek();
+            st.push(i);
+
+
+        }
+
+        int largest=Integer.MIN_VALUE;
+        int sum;
+
+        for (int i=0;i<n;i++){
+
+            sum=heights[i]*(nse[i]-pse[i]-1);
+            largest=Math.max(largest,sum);
+
+
+
+        }
+
+        return largest;
+
+
+    }
+
     public static void main(String[] args) {
+
+        int[] arr={2,1,5,6,2,3};
+        System.out.println(largestRectangleHistogram(arr));
 
 
     }
