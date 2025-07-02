d82fd45bdda086d80205b360d3b7c4a56a2e9a3f
Tushar Patra <tushar04012002@gmail.com>
2025-07-03 00:08:41 +0530

    dsa

--- src/myPractice.java
+++ src/myPractice.java
@@ -1,108 +1,10 @@
-import java.util.EmptyStackException;
+
 
 public class myPractice {
-
-    public static class Stack {
-
-
-        int idx = -1;
-        int[] stack = new int[10];
-
-
-        public void push(int val) {
-
-            //Time Complexity: O(1)
-            //Space Complexity: O(1)
-
-            if (idx == stack.length - 1) {
-                int[] dynamicArray=new int[2*stack.length];
-
-                for(int i=0;i<stack.length;i++){
-                    dynamicArray[i]=stack[i];
-                }
-
-                stack=dynamicArray;
-            }
-
-            idx++;
-            stack[idx] = val;
-
-
-        }
-
-        public int peek() {
-
-            //Time Complexity: O(1)
-            //Space Complexity: O(1)
-
-
-            if (idx == -1) {
-                throw new EmptyStackException();
-            }
-
-            return stack[idx];
-        }
-
-
-        public int pop() {
-
-            //Time Complexity: O(1)
-            //Space Complexity: O(1)
-
-
-            if (idx == -1) {
-                throw new EmptyStackException();
-            }
-
-            int top = stack[idx];
-            idx--;
-
-            return top;
-
-
-        }
-
-        public int size() {
-
-            //Time Complexity: O(1)
-            //Space Complexity: O(1)
-
-
-            if (idx == -1) return 0;
-
-            return idx + 1;
-        }
-
-        boolean isEmpty() {
-
-            //Time Complexity: O(1)
-            //Space Complexity: O(1)
-
-
-            if (idx == -1) return true;
-            else return false;
-
-
-        }
-
-        void display() {
-
-            //Time Complexity: O(n)
-            //Space Complexity: O(1)
-
-
-            for (int i = 0; i <= idx; i++) {
-                System.out.print(stack[i] + " ");
-            }
-
-            System.out.println();
-        }
-    }
 
     public static void main(String[] args) {
 
     }
-
 
 
 }
