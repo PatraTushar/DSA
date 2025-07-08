956174db0c5a93c4fce0f5860d904920d6547b53
Tushar Patra <tushar04012002@gmail.com>
2025-07-08 21:27:22 +0530

    dsa

--- src/Java_Collection_FrameWorkk/QueueInterface.java
+++ src/Java_Collection_FrameWorkk/QueueInterface.java
@@ -14,5 +14,30 @@ public class QueueInterface {
 
         System.out.println(q.poll()); // A — removed from front
         System.out.println(q);        // [B]
+
+        // ArrayDeque
+
+        Deque<String> dq = new ArrayDeque<>();
+        dq.offer("A");
+        dq.offer("B");
+
+        System.out.println(dq.poll());   // A
+        dq.offerFirst("X");              // Add to front
+        dq.offerLast("Y");               // Add to rear
+
+        System.out.println(dq);         // [X, B, Y]
+
+
+        // PriorityQueue
+
+        Queue<Integer> pq = new PriorityQueue<>();
+        pq.offer(30);
+        pq.offer(10);
+        pq.offer(20);
+
+        System.out.println(pq.poll()); // 10 — smallest first
+        System.out.println(pq);        // Not necessarily sorted when printed
+
+
     }
 }
