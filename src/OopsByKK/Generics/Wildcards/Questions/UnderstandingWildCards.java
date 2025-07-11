4ded70c9a39407d8aacfb7289a8084aaededda50
Tushar Patra <tushar04012002@gmail.com>
2025-07-11 23:23:30 +0530

    dsa

--- src/OopsByKK/Generics/Wildcards/Questions/UnderstandingWildCards.java
+++ src/OopsByKK/Generics/Wildcards/Questions/UnderstandingWildCards.java
@@ -96,6 +96,51 @@ public class UnderstandingWildCards {
     // Q6->Write a method copyList that copies all elements from a source list to a destination list using proper wildcards.
 
 
+    public static <T> void copyList(List<? super T> dest, List<? extends T> src) {
+        for (T item : src) {
+            dest.add(item); // read from src, write to dest
+        }
+
+
+        // not possible bcoz In subtype (? extends T), we can read but can't write.
+        //Because adding a supertype (like Number) to a subtype list (like List<Integer>) would break type safety.
+//        for(T item:dest){
+//            src.add(item);
+//        }
+
+
+    }
+
+
+    // Q7->Write a method that accepts a list of unknown type using <?> and prints all elements.
+
+   // Also, explain why <?> is useful even though the type is unknown ?
+    //“Since <?> denotes an unknown type, and all Java reference types are subclasses of Object, we can safely iterate over the list using Object as the loop variable type. This works because reading from a list with an unknown type is type-safe, even though adding elements is not.”
+
+
+    // Q8->Write a method that accepts a list of numbers and prints their double value.
+    //The method should work with List<Integer>, List<Double>, List<Float>, etc., using wildcards.
+
+    static void print(List<? extends Number> list){
+
+        for(Number ele:list){
+            System.out.println(ele.doubleValue());
+        }
+    }
+
+
+    static void printUnknownList(List<?> list){
+
+        for(Object obj:list){
+            System.out.println(obj);
+        }
+    }
+
+    //Q9-> Why can’t you add elements to a List<? extends T>? Explain with code and reasoning.
+  //  "Before the method call, the compiler knows the exact generic type of the list, so it knows what elements can be added safely.
+    //But after the method call, when the list is treated as List<? extends T>, the exact subtype becomes unknown. Since adding any specific element might violate type safety, the compiler disallows adding anything (except null) to prevent runtime errors."
+
+
 
 
 
@@ -118,6 +163,25 @@ public class UnderstandingWildCards {
         addInteger(list4);
         List<Integer> list5=new ArrayList<>();
         addInteger(list5);
+
+
+        List<Integer> src = List.of(10, 20, 30);
+        List<Number> dest = new ArrayList<>();
+
+        copyList(dest, src);
+
+        System.out.println(dest); // Output: [10, 20, 30]
+
+
+        List<String> obj=List.of("Kunal","Rohit");
+        printUnknownList(obj);
+        List<Character> obj2=List.of('A','B','C');
+        printUnknownList(obj2);
+
+
+        List<Integer> obj3=List.of(1,2,3);
+        print(obj3);
+
 
 
 
--- src/myPractice.java
+++ src/myPractice.java
@@ -1,9 +1,34 @@
-import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.List;
 
 public class myPractice {
 
+    static <T> void copy(List<? super T> dest,List<? extends T > src){
+
+        //“In the parameter, it will first check the extends type — what is the type?”
+       // “The T is Integer, so it will go and replace the T of super automatically.”
+        //“As dest is of type Number, it can accept Integer.”
+
+        //"Where you are adding (i.e., dest), always try to keep it as a supertype, so adding a subtype is possible."
+
+
+        for(T ele :src){
+
+            dest.add(ele);
+        }
+
+        System.out.println(dest);
+
+
+    }
+
     public static void main(String[] args) {
 
+        List<Integer> src=List.of(1,2,3,4,5);
+        List<Number> dest=new ArrayList<>();
+
+
+        copy(dest,src);
 
 
     }
