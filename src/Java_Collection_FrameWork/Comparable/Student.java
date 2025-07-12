78b2c0674e602fa6d33d88560e52c5dddba5d0fa
Tushar Patra <tushar04012002@gmail.com>
2025-07-12 23:20:53 +0530

    dsa

--- src/Java_Collection_FrameWork/Comparable/Student.java
+++ src/Java_Collection_FrameWork/Comparable/Student.java
@@ -16,26 +16,27 @@ public class Student implements Comparable<Student> {
 
     }
 
-    public int compareTo(Student s){
+    public int compareTo(Student s) {
 
-        return this.marks-s.marks;
+
+        return this.marks - s.marks;
     }
 
-    public String toString(){
+    public String toString() {
 
-        return this.name +" "+this.marks;
+        return this.name + " " + this.marks;
     }
 
     public static void main(String[] args) {
 
-        Student[] arr=new Student[]{new Student("rahul",100),new Student("Ayush",80),new Student("charles",90)};
+        Student[] arr = new Student[]{new Student("rahul", 100), new Student("Ayush", 80), new Student("charles", 90)};
         //“We passed Student[] into Arrays.sort(), so internally T is replaced by Student.”
         Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
 
 
-        List<Student> list=new ArrayList<>();
-        list.add(new Student("Ram",90));
+        List<Student> list = new ArrayList<>();
+        list.add(new Student("Ram", 90));
         list.add(new Student("Shyam", 80));
         list.add(new Student("Mohan", 95));
 
---
+++ src/myPractice.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class myPractice {
+
+    public static void main(String[] args) {
+
+     int[] arr={1,5,3,6};
+     Arrays.sort(arr);
+
+    }
+}
