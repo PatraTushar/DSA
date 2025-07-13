2d3893872601c13dd38d171bd5f485f6b4b17c88
Tushar Patra <tushar04012002@gmail.com>
2025-07-14 03:38:21 +0530

    dsa

---
+++ src/strings/InterviewQuestions/Q10.java
@@ -0,0 +1,39 @@
+package strings.InterviewQuestions;
+
+public class Q10 {
+
+    static boolean detectCapitalUse(String s) {
+
+        // Time Complexity: O(N)
+        //Space Complexity: O(1)
+
+
+        char[] ch = s.toCharArray();
+        int uppercase = 0;
+
+        for (int i = 0; i < ch.length; i++) {
+            if (Character.isUpperCase(ch[i])) uppercase++;
+        }
+
+
+        if (uppercase == s.length()) return true;
+
+        if (uppercase == 0) return true;
+
+
+        if (uppercase == 1 && Character.isUpperCase(ch[0])) return true;
+
+
+        return false;
+
+
+    }
+
+    public static void main(String[] args) {
+
+        String word = "USA";
+        System.out.println(detectCapitalUse(word));
+
+
+    }
+}
