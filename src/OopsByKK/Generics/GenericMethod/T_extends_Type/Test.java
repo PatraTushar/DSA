bad3f3170c1f4a38653a131aee3ae53821b9f24f
Tushar Patra <tushar04012002@gmail.com>
2025-07-16 22:50:04 +0530

    dsa

---
+++ src/OopsByKK/Generics/GenericMethod/T_extends_Type/Test.java
@@ -0,0 +1,41 @@
+package OopsByKK.Generics.GenericMethod.T_extends_Type;
+
+interface Animal{
+
+    void speak();
+
+}
+
+interface RunnableAnimal{
+
+    void run();
+}
+
+
+class Dog implements Animal,RunnableAnimal{
+
+    public void speak(){
+        System.out.println(" woof ");
+    }
+
+    public void run(){
+
+        System.out.println(" Run fast ");
+    }
+}
+public class Test {
+
+    public <T extends Animal & RunnableAnimal> void play(T animal){
+
+        animal.speak();
+        animal.run();
+    }
+
+    public static void main(String[] args) {
+
+        new Test().play(new Dog());
+
+
+
+    }
+}
