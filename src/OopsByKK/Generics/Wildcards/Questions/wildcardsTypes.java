73052edeea6ab948f688da4c7806a7f02bf07a6e
Tushar Patra <tushar04012002@gmail.com>
2025-07-24 03:05:32 +0530

    dsa

---
+++ src/OopsByKK/Generics/Wildcards/Questions/wildcardsTypes.java
@@ -0,0 +1,15 @@
+package OopsByKK.Generics.Wildcards.Questions;
+
+public class wildcardsTypes {
+
+
+
+     // 1. <?> → Unbounded Wildcard
+
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
--- src/myPractice.java
+++ src/myPractice.java
@@ -1,30 +1,43 @@
-import java.util.ArrayList;
-import java.util.List;
+import OopsByKK.AccessModifier.Public.A;
+
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class myPractice {
 
+    static class Student {
+
+        String name;
+        int marks;
+
+        Student(String name, int marks) {
+
+            this.name = name;
+            this.marks = marks;
+        }
 
 
+        public String toString() {
 
-    static void add(List<? super Number> list){
+            return this.name + " " + this.marks;
+        }
 
-      list.add(1.5);
-      list.add(2);
-        System.out.println(list);
+    }
+
+    static class ClassComparator implements Comparator<Student> {
+
+        @Override
+        public int compare(Student o1, Student o2) {
+            return o1.name.compareTo(o2.name);
+        }
     }
 
 
     public static void main(String[] args) {
 
-        List<String> obj= List.of("raj","rahul");
-
-        List<Number> obj2=new ArrayList<>();
-        obj2.add(1);
-
-       List<Double> obj3=List.of(1.2,2.2,3.3);
-
-        add(obj2);
-
+        Student[] arr = new Student[]{new Student("rohan", 12), new Student("bob", 1), new Student("alan", 25)};
+        Arrays.sort(arr,new ClassComparator());
+        System.out.println(Arrays.toString(arr));
 
 
     }
