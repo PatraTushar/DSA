b97aeb975e70434ba4b358d27778ed137a858353
Tushar Patra <tushar04012002@gmail.com>
2025-07-26 23:24:52 +0530

    dsa

--- src/RecursionByStriver/StriverPlaylist/InterviewQuestions/Q12.java
+++ src/RecursionByStriver/StriverPlaylist/InterviewQuestions/Q12.java
@@ -14,7 +14,7 @@ public class Q12 {
         Arrays.sort(nums);
         List<List<Integer>> outer = new ArrayList<>();
         outer.add(new ArrayList<>());
-        int start ;
+        int start;
         int end = 0;
         for (int i = 0; i < nums.length; i++) {
             start = 0;
@@ -46,13 +46,57 @@ public class Q12 {
 
     // RECURSIVE APPROACH
 
+    static List<List<Integer>> subsetsWithoutDuplicatesI(int[] arr) {
 
+        // Time Complexity: O(n log n + 2^n)
+        // Space Complexity: O(2ⁿ × n)   2^n → number of subset    //n → max length of each subset (for copy)
+
+        Arrays.sort(arr);
+
+        return Helper(arr, 0, new ArrayList<>(), new ArrayList<>());
+
+
+    }
+
+    static List<List<Integer>> Helper(int[] arr, int index, List<Integer> subsets, List<List<Integer>> result) {
+
+        if (index == arr.length) {
+            result.add(new ArrayList<>(subsets));
+            return result;
+        }
+
+
+        subsets.add(arr[index]);
+        Helper(arr, index + 1, subsets, result);
+
+
+
+        int nextIndex=index+1;
+
+        while (nextIndex<arr.length && arr[index]==arr[nextIndex]){
+            nextIndex++;
+        }
+
+
+        subsets.remove(subsets.size()-1);
+        Helper(arr,nextIndex,subsets,result);
+
+
+
+
+        return result;
+
+
+    }
 
     public static void main(String[] args) {
 
-        int[] arr={1,2,2};
-        List<List<Integer>> ans=subsetWithNoDuplicates(arr);
+        int[] arr = {1, 2, 2};
+        List<List<Integer>> ans = subsetWithNoDuplicates(arr);
         System.out.println(ans);
+
+        List<List<Integer>> ans1 = subsetsWithoutDuplicatesI(arr);
+        System.out.println(ans1);
 
 
     }
