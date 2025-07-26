b6df6d7c907739d01b5b0c22db1da777f5aa1a45
Tushar Patra <tushar04012002@gmail.com>
2025-07-26 15:47:00 +0530

    dsa

--- src/myPractice.java
+++ src/myPractice.java
@@ -1,8 +1,34 @@
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class myPractice {
 
+    static ArrayList<Integer> helper(int[] arr, int index, int sum, ArrayList<Integer> list){
+
+        if(index==arr.length){
+            list.add(sum);
+           // Collections.sort(list);
+            return list;
+        }
+
+        helper(arr,index+1,sum,list);
+        helper(arr,index+1,sum+arr[index],list);
+
+
+        return list;
+
+    }
+
+    static ArrayList<Integer> subsetsum(int[] arr){
+
+        return helper(arr,0,0,new ArrayList<>());
+    }
+
+
+
     public static void main(String[] args) {
+
+
 
 
     }
