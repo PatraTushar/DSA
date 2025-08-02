7da251de12f9f09a7829119ebc7c2350eded5272
Tushar Patra <tushar04012002@gmail.com>
2025-08-02 23:30:21 +0530

    dsa

--- src/OopsByKK/Interfaces/NestedInterface/StaticInterfaceInsideAClass/Main.java
+++ src/OopsByKK/Interfaces/NestedInterface/StaticInterfaceInsideAClass/Main.java
@@ -1,27 +1,28 @@
 package OopsByKK.Interfaces.NestedInterface.StaticInterfaceInsideAClass;
 
 
-class outer{
+class outer {
 
     // static interface inside a class
-    public interface NestedInterface{
+    public interface NestedInterface {
 
         void display();
 
     }
 }
 
-class implementer implements outer.NestedInterface{
+class implementer implements outer.NestedInterface {
 
-    public void display(){
+    public void display() {
         System.out.println("Implemented Nested Interface inside a class");
     }
 }
+
 public class Main {
 
     public static void main(String[] args) {
 
-        implementer obj=new implementer();
+        implementer obj = new implementer();
         obj.display();
 
     }
--- src/myPractice.java
+++ src/myPractice.java
@@ -1,19 +1,66 @@
-import OopsByKK.Generics.GenricClass.Pair;
+
 
 public class myPractice {
 
+    interface dog {
 
-    public static class parent {
+        void bark();
+
+        default void dance(){
+
+            System.out.println(" dog dance ");
 
 
-        public static final void eat(){
+        }
+    }
 
-            System.out.println(" ");
+    interface cat extends dog {
 
+        void meow();
+
+
+
+    }
+
+
+    public static class Animal implements cat {
+
+        @Override
+        public void bark() {
+
+            System.out.println(" Animal bark ");
+        }
+
+        @Override
+        public void meow() {
+
+            System.out.println(" Animal meow ");
         }
 
 
+
+    }
+
+    public static void main(String[] args) {
+
+        dog obj=new Animal();
+        obj.bark();
+        obj.dance();
+
+        cat obj2=new Animal();
+        obj2.meow();
+        obj2.bark();
+        obj2.dance();
+
+
+        Animal obj3=new Animal();
+        obj3.bark();
+        obj3.meow();
+        obj3.dance();
+
+
+
     }
 
 
-    }
+}
