2d23077efe4994a521aca4ad10ec31e1e4449a36
Tushar Patra <tushar04012002@gmail.com>
2025-08-05 01:09:30 +0530

    dsa

--- src/myPractice.java
+++ src/myPractice.java
@@ -1,8 +1,40 @@
+import OopsByKK.SingletonClass.Singleton;
+
+import java.util.*;
 
 public class myPractice {
 
 
+    public static void printList(List<? extends Number> list) {
+
+
+
+        for (Object ele : list) {
+
+            System.out.println(ele);
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+        List<Double> list = Arrays.asList(1.2, 3.4, 5.6);
+        List<Integer> list1 = Arrays.asList(1, 2, 3, 4);
+        myPractice.printList(list);
+        myPractice.printList(list1);
+
+        List<Integer> list2 = new ArrayList<>();
+        myPractice.printList(list2);
+
+
+    }
+
 }
+
+
+
+
+
 
 
 
