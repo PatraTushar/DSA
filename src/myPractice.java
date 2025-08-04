86e8dfcf0f92a320f8d30ba4042719d3d938ceea
Tushar Patra <tushar04012002@gmail.com>
2025-08-04 20:34:36 +0530

    dsa

--- src/myPractice.java
+++ src/myPractice.java
@@ -1,142 +1,33 @@
+import java.util.Arrays;
+
+class Animal {
+
+    void eat() {
+
+        System.out.println(" Animal eats ");
+    }
+}
+
+class Dog extends Animal implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println(" Dog is running ");
+    }
+}
+
+
 
 
 public class myPractice {
 
 
-    public static class ArrayList<T> {
+    public static class utilities{
 
-        T[] arr;
+        public static <T extends Animal & Runnable> void process(T obj){
 
-        int size;
-
-        int capacity;
-
-        public static final int DEFAULT_CAPACITY = 10;
-
-
-        @SuppressWarnings("unchecked")
-        ArrayList() {
-
-            this.size = 0;
-            this.capacity = DEFAULT_CAPACITY;
-            this.arr = (T[]) new Object[capacity];
-        }
-
-
-
-        @SuppressWarnings("unchecked")
-        private void resize() {
-
-            capacity*=2;
-            T[] newArr=(T[]) new Object[capacity];
-
-            for(int i=0;i<size;i++){
-
-                newArr[i]=arr[i];
-            }
-
-            arr=newArr;
-
-
-        }
-
-
-        public void add(T val) {
-
-            if (size == capacity) {
-
-                resize();
-            }
-
-            arr[size++] = val;
-
-
-        }
-
-        public void remove(int index) {
-
-            if (index < 0 || index >=size) {
-
-                throw new IndexOutOfBoundsException(" Not a valid index ");
-
-            }
-
-
-
-            for(int i=index;i<size-1;i++){
-
-                arr[i]=arr[i+1];
-            }
-
-
-          size--;
-
-
-            if(size<capacity/4 && capacity>DEFAULT_CAPACITY){
-
-                shrink();
-            }
-
-
-        }
-
-
-        @SuppressWarnings("unchecked")
-        private void shrink(){
-
-            capacity/=2;
-
-            T[] newArr=(T[]) new Object[capacity];
-
-            for(int i=0;i<size;i++){
-
-                newArr[i]=arr[i];
-            }
-
-            arr=newArr;
-        }
-
-        public Object get(int index){
-
-            if (index < 0 || index >=size) {
-
-                throw new IndexOutOfBoundsException(" Not a valid index ");
-
-            }
-
-            return arr[index];
-
-
-        }
-
-
-        public int getSize() {
-            return size;
-        }
-
-        public boolean isEmpty(){
-
-            if(size<=0) return true;
-
-            return false;
-        }
-
-        public String toString(){
-
-            StringBuilder str=new StringBuilder();
-
-            str.append("[");
-
-            for(int i=0;i<size;i++){
-
-                str.append(arr[i]);
-
-                if(i!=size-1) str.append(",");
-            }
-
-            str.append("]");
-
-            return str.toString();
+            obj.run();
+            obj.eat();
 
 
         }
@@ -144,19 +35,7 @@ public class myPractice {
 
     public static void main(String[] args) {
 
-        ArrayList<Integer> obj=new ArrayList<>();
-        obj.add(10);
-        obj.add(100);
-        obj.add(500);
-
-        System.out.println(obj.toString());
-
-//
-//        Integer get1=(Integer) obj.get(0);
-//        String  get2=(String) obj.get(0);
-//        Character get3=(Character) obj.get(0);
-
-
+        utilities.process(new Dog());
     }
 
 
