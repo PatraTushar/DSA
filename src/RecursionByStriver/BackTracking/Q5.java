6d0102b96180f2b8c88fd8f4b4fe7d5369a0ff9b
Tushar Patra <tushar04012002@gmail.com>
2025-08-06 20:59:23 +0530

    dsa

--- src/RecursionByStriver/BackTracking/Q5.java
+++ src/RecursionByStriver/BackTracking/Q5.java
@@ -4,6 +4,72 @@ import java.util.*;
 
 public class Q5 {
 
+    // BRUTEFORCE APPROACH
+    static String convertToString(int n) {
+
+
+
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= n; i++) {
+
+            sb.append(i);
+        }
+
+        return sb.toString();
+    }
+
+    static List<String> permutations(String str, String Ans, List<String> list) {
+
+        if (str.isEmpty()) {
+            list.add(Ans);
+            return list;
+        }
+
+        char ch = str.charAt(0);
+
+        for (int i = 0; i <= Ans.length(); i++) {
+
+            String first = Ans.substring(0, i);
+            String second = Ans.substring(i);
+            permutations(str.substring(1), first + ch + second, list);
+
+        }
+
+        return list;
+
+
+    }
+
+    static String kthPermutation(int n, int k) {
+
+        // Time Complexity: O(n! × n + n! log n!)
+        //Space Complexity: O(n!)
+
+        String ans = convertToString(n);
+
+        List<String> list = permutations(ans, "", new ArrayList<>());
+
+        Collections.sort(list);
+
+        for (int i = 0; i < list.size(); i++) {
+
+            if (i == k - 1) {
+
+                return list.get(i);
+
+
+            }
+
+
+        }
+
+
+        return "";
+
+    }
+
 
     // optimal solution
     static String getPermutation(int n,int k){
--- src/myPractice.java
+++ src/myPractice.java
@@ -1,104 +1,89 @@
-import OopsByKK.SingletonClass.Singleton;
-
-import java.util.*;
+import javax.swing.event.ListDataListener;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class myPractice {
 
 
-    public static class Wildcards {
+    static String convertToString(int n) {
 
-        public static void printList(List<?> list) {
 
-            for (Object ele : list) {
 
-                System.out.print(ele + " ");
-            }
 
-            System.out.println();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= n; i++) {
+
+            sb.append(i);
         }
 
-        public static int sumOfNumber(List<? extends Number> list) {
-
-
-            int sum = 0;
-
-            for (Number ele : list) {
-
-                sum += ele.intValue();
-
-            }
-
-            return sum;
-
-
-        }
-
-        public static void addInteger(List<? super Integer> list) {
-
-            for (int i = 1; i <= 5; i++) {
-
-                list.add(i);
-            }
-
-            System.out.println(list);
-
-
-        }
-
-        public static <T> void copyList(List<? super T> dest,List<? extends T> src){
-
-            for(T ele:src){
-
-                dest.add(ele);
-            }
-
-            System.out.println(dest);
-        }
-
-        public static void print(List<? extends Number> list){
-
-            for(Number ele:list){
-
-                System.out.print(ele.doubleValue() +" ");
-            }
-
-            System.out.println();
-
-
-
-        }
+        return sb.toString();
     }
+
+    static List<String> permutations(String str, String Ans, List<String> list) {
+
+        if (str.isEmpty()) {
+            list.add(Ans);
+            return list;
+        }
+
+        char ch = str.charAt(0);
+
+        for (int i = 0; i <= Ans.length(); i++) {
+
+            String first = Ans.substring(0, i);
+            String second = Ans.substring(i);
+            permutations(str.substring(1), first + ch + second, list);
+
+        }
+
+        return list;
+
+
+    }
+
+    static String kthPermutation(int n, int k) {
+
+        // Time Complexity: O(n! × n + n! log n!)
+        //Space Complexity: O(n!)
+
+        String ans = convertToString(n);
+
+        List<String> list = permutations(ans, "", new ArrayList<>());
+
+        Collections.sort(list);
+
+        for (int i = 0; i < list.size(); i++) {
+
+            if (i == k - 1) {
+
+                return list.get(i);
+
+
+            }
+
+
+        }
+
+
+        return "";
+
+    }
+
 
     public static void main(String[] args) {
 
-
-        List<Integer> list1 = Arrays.asList(1, 2, 3, 4);
-        List<String> list2 = Arrays.asList("a", "b", "c");
-        List<Double> list3 = Arrays.asList(1.1, 2.3, 4.5);
-
-        Wildcards.printList(list1);
-        Wildcards.printList(list2);
-        System.out.println(Wildcards.sumOfNumber(list1));
-        System.out.println(Wildcards.sumOfNumber(list3));
-
-        List<Integer> list4 = new ArrayList<>();
-        List<Object> list5 = new ArrayList<>();
-        List<Number> list6 = new ArrayList<>();
-        List<Double> list7 = new ArrayList<>();
-
-//        Wildcards.addInteger(list4);
-//        Wildcards.addInteger(list5);
-//        Wildcards.addInteger(list6);
-
-        Wildcards.copyList(list6,list1);
-
-        Wildcards.print(list1);
+        int n = 3;
+        int k = 3;
 
 
+        System.out.println();
 
+        String  result=kthPermutation(n,k);
+        System.out.println(result);
 
     }
-
 
 }
 
