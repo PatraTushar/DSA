3724d60d4badf1d55115d4d98696a0a7b769f9d3
Tushar Patra <tushar04012002@gmail.com>
2025-08-11 20:46:33 +0530

    dsa

--- src/OopsByKK/streamAPI/LambdaExpression/function.java
+++ src/OopsByKK/streamAPI/LambdaExpression/function.java
@@ -13,16 +13,22 @@ public class function {
         // function->work for you
         //  Function<Input,output>
 
-        Function<Integer,Integer> doubleIt=(e)->2*e;
-        Function<Integer,Integer> tripleIt=(e)->3*e;
+        Function<Integer, Integer> doubleIt = (e) -> 2 * e;
+        Function<Integer, Integer> tripleIt = (e) -> 3 * e;
         System.out.println(doubleIt.apply(100));
-        System.out.println( doubleIt.andThen(tripleIt).apply(40));
-        System.out.println( doubleIt.compose(tripleIt).apply(30));
-       Function<Integer,Integer> obj= Function.identity();
+
+        // andThen : first double, then triple
+        System.out.println(doubleIt.andThen(tripleIt).apply(40));
+
+        // compose means : first tripeIt, then doubleIt
+        System.out.println(doubleIt.compose(tripleIt).apply(30));
+
+        // identity(): returns the same value that is passed in as input
+        Function<Integer, Integer> obj = Function.identity();
         System.out.println(obj.apply(85));
 
-       // Function<Integer,Integer> a=(e)->2*e; --> UnaryOperator<Integer> v=(e)->2*e;  same meaning
-        UnaryOperator<Integer> v=(e)->2*e;
+        // Function<Integer,Integer> a=(e)->2*e; --> UnaryOperator<Integer> v=(e)->2*e;  same meaning
+        UnaryOperator<Integer> v = (e) -> 2 * e;   // If a Function<T, R> has the same generic type for both T and R (Function<T, T>), you can replace it with UnaryOperator<T>.
         System.out.println(v.apply(2000));
 
         //Example 1: Square a number
@@ -32,21 +38,21 @@ public class function {
         System.out.println(square.apply(5)); // Output: 25
 
         // Example 2: Length of number in digits
-        Function<Integer,Integer> numLength=(e)->String.valueOf(e).length();
+        Function<Integer, Integer> numLength = (e) -> String.valueOf(e).length();
         System.out.println(numLength.apply(12345));
 
         //  Example 3: Chain Functions (Advanced)
 
-        Function<Integer,Integer> firstDoubleIt=(e)->2*e;
-        Function<Integer,Integer> TripleIt=(e)->3*e;
+        Function<Integer, Integer> firstDoubleIt = (e) -> 2 * e;
+        Function<Integer, Integer> TripleIt = (e) -> 3 * e;
 
         System.out.println(doubleIt.andThen(tripleIt).apply(30));
 
         // Real-World Usage
         //Example: Stream .map() uses Function<T, R>
 
-        List<Integer> numbers= Arrays.asList(1,2,3,4);
-        Function<Integer,Integer> cube=(e)->e*e*e;
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4);
+        Function<Integer, Integer> cube = (e) -> e * e * e;
 
         System.out.println(numbers.stream().map(cube).collect(Collectors.toList()));
 
