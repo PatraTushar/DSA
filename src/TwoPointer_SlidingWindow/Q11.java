3dc55d584af719675403ea2baec22aefd1c31701
Tushar Patra <tushar04012002@gmail.com>
2025-08-19 12:55:46 +0530

    dsa

---
+++ src/TwoPointer_SlidingWindow/Q11.java
@@ -0,0 +1,51 @@
+package TwoPointer_SlidingWindow;
+
+public class Q11 {
+
+
+    static int subArraysWithSumLessThanEqualToGoal(int[] arr, int goal) {
+
+        if (goal < 0) return 0;
+
+        int n = arr.length;
+        int count = 0;
+        int left = 0;
+        int sum = 0;
+
+        for (int right = 0; right < n; right++) {
+
+            sum += arr[right];
+
+
+            while (sum > goal) {
+
+                sum -= arr[left];
+                left++;
+            }
+
+            count += (right - left + 1);
+
+
+        }
+
+        return count;
+    }
+
+    static int subArraysWithSum(int[] arr, int goal) {
+
+        //  Time Complexity: O(n)
+        //  Space Complexity: O(1)
+
+        return subArraysWithSumLessThanEqualToGoal(arr, goal) - subArraysWithSumLessThanEqualToGoal(arr, goal - 1);
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 0, 1, 0, 1};
+        int goal = 2;
+        System.out.println(subArraysWithSum(arr, goal));
+
+
+    }
+}
--- src/myPractice.java
+++ src/myPractice.java
@@ -2,26 +2,42 @@
 
 public class myPractice {
 
-    public static class math{
+    static int subArraysWithSum(int[] nums, int goal) {
+        return subArraysWithSumLessThanEqualToGoal(nums, goal) - subArraysWithSumLessThanEqualToGoal(nums, goal - 1);
+    }
 
-         void add(int a,int b){
 
-             System.out.println(a+b);
-         }
+    static int subArraysWithSumLessThanEqualToGoal(int[] nums, int goal) {
+
+        if (goal < 0) return 0;
+
+        int n = nums.length;
+        int count = 0;
+        int left = 0;
+        int sum = 0;
+
+        for (int right = 0; right < n; right++) {
+
+            sum += nums[right];
+
+
+            while (sum > goal) {
+
+                sum -= nums[left];
+                left++;
+            }
+
+            count += (right - left + 1);
+
+
+        }
+
+        return count;
     }
 
 
 
     public static void main(String[] args) {
-
-        math obj=new math(){
-
-            void add(int a,int b){
-                System.out.println(2*a+2*b);
-            }
-        };
-
-        obj.add(2,3);
 
 
     }
