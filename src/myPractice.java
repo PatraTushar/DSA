38a52c8dca2487c12eae569925adfbd4a9e4ce68
Tushar Patra <tushar04012002@gmail.com>
2025-08-23 23:03:21 +0530

    dsa

--- src/myPractice.java
+++ src/myPractice.java
@@ -1,27 +1,13 @@
-
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class myPractice {
 
 
-
     public static void main(String[] args) {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
