ece57e64ef88e78f29d904754d0a9e0f2bcd19fa
Tushar Patra <tushar04012002@gmail.com>
2025-08-26 00:54:14 +0530

    dsa

--- src/BitManipulation/InterviewQuestions/Q7.java
+++ src/BitManipulation/InterviewQuestions/Q7.java
@@ -2,25 +2,54 @@ package BitManipulation.InterviewQuestions;
 
 public class Q7 {
 
-    static int divide(int dividend,int divisor){
+    static int divide(int dividend, int divisor) {
+
+        // Time Complexity: O(log(|dividend| / |divisor|))
+        // Space Complexity: O(1)
 
 
-        int sum=0;
-        int count=0;
+        if (dividend == Integer.MIN_VALUE && divisor == -1) return Integer.MAX_VALUE;
+        if (dividend == Integer.MIN_VALUE && divisor == 1) return Integer.MIN_VALUE;
 
-        while (sum+divisor<=dividend){
-            count++;
-            sum+=divisor;
+        long dd = Math.abs((long) dividend);
+        long dv = Math.abs((long) divisor);
+
+        int res = 0;
+
+        while (dd>=dv) {
+
+            long sum = dv;
+            int count = 1;
+
+            while (sum + sum <= dd) {
+
+
+                sum += sum;
+                count += count;
+            }
+
+
+            res += count;
+            dd -= sum;
+
+
         }
 
-        return count;
+        if ((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)) return -res;
+
+        return res;
+
+
     }
 
     public static void main(String[] args) {
 
-        int dividend=10;
-        int divisor=3;
-        System.out.println(divide(dividend,divisor));
+        // leeTCode->29
+
+        int dividend = 120;
+        int divisor = 3;
+        System.out.println(divide(dividend, divisor));
+        System.out.println(divide(10, divisor));
 
 
     }
