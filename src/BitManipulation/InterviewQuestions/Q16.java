692ab3d9a90ccb617300360d137335bd8f15f156
Tushar Patra <tushar04012002@gmail.com>
2025-09-02 17:02:03 +0530

    dsa

--- src/BitManipulation/InterviewQuestions/Q16.java
+++ src/BitManipulation/InterviewQuestions/Q16.java
@@ -3,22 +3,45 @@ package BitManipulation.InterviewQuestions;
 public class Q16 {
 
 
+    // BRUTE FORCE APPROACH
+    static int rangeBitwise1(int left, int right) {
+
+        //  Time Complexity: O(right-left)
+        //  Space Complexity: O(1)
+
+
+        int result = left;
+        for (int i = left + 1; i <= right; i++) {
+
+            result = result & i;
+
+            if(result==0) break;
+
+
+        }
+
+        return result;
+
+
+    }
+
+
     static int rangeBitwiseEnd(int left, int right) {
 
         //  Time Complexity: O(32)->O(1)
         //  Space Complexity: O(1)
 
 
-        int shiftCount=0;
+        int shiftCount = 0;
 
-        while (left!=right){
+        while (left != right) {
 
-            left=left>>1;
-            right=right>>1;
+            left = left >> 1;
+            right = right >> 1;
             shiftCount++;
         }
 
-        return left<<shiftCount;
+        return left << shiftCount;
     }
 
     public static void main(String[] args) {
@@ -28,8 +51,9 @@ public class Q16 {
 
         int left = 5;
         int right = 7;
-        System.out.println(rangeBitwiseEnd(left,right));
-        System.out.println(rangeBitwiseEnd(10,13));
+        System.out.println(rangeBitwiseEnd(left, right));
+        System.out.println(rangeBitwiseEnd(10, 13));
+        System.out.println(rangeBitwise1(10, 13));
 
     }
 }
