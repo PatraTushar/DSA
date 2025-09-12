34c978897644844f78ad3415f73cb9f21515e732
Tushar Patra <tushar04012002@gmail.com>
2025-09-13 01:13:32 +0530

    dsa

--- src/ArraysbyStriver/interviewQuestions/Q23.java
+++ src/ArraysbyStriver/interviewQuestions/Q23.java
@@ -8,26 +8,24 @@ public class Q23 {
     //Time Complexity: O(n^2)
     //Space Complexity: O(n^2)
 
-    static int[][] pascal(int n){
+    static int[][] pascal(int n) {
 
-      int[][] ans=new int[n][];
-
-
-        for(int i=0;i<n;i++){
-
-            ans[i]=new int[i+1];
-            ans[i][0]=ans[i][i]=1;
+        int[][] ans = new int[n][];
 
 
-            for(int j=1;j<i;j++){
+        for (int i = 0; i < n; i++) {
 
-                ans[i][j]=ans[i-1][j]+ans[i-1][j-1];
+            ans[i] = new int[i + 1];
+            ans[i][0] = ans[i][i] = 1;
 
+
+            for (int j = 1; j < i; j++) {
+
+                ans[i][j] = ans[i - 1][j] + ans[i - 1][j - 1];
 
 
             }
         }
-
 
 
         return ans;
@@ -35,70 +33,71 @@ public class Q23 {
 
     }
 
-    static int pascalTriangleI(int row,int col){   //  row=5 col=3
+    static int pascalTriangleI(int row, int col) {   //  row=5 col=3
 
+
+        // Time Complexity: O(c)
+        //  Space Complexity: O(1)
 
         // TYPE 1->Given row and col and tell the element at that place
 
-        int n=row-1;
-        int c=col-1;
+        int n = row - 1;
+        int c = col - 1;
 
-        int  result=1;
+        int result = 1;
 
-        for(int i=0;i<c;i++){
+        for (int i = 0; i < c; i++) {
 
-            result=result*(n-i);
-            result=result/(i+1);
+            result = result * (n - i);
+            result = result / (i + 1);
         }
 
         return result;
 
 
-
-
     }
 
 
-    static List<Integer> pascalTriangleII(int n){
+    static List<Integer> pascalTriangleII(int n) {
 
-       // Type 2->Print ant nth row of pascal triangle
+        // Type 2->Print the nth row of pascal triangle
 
-        List<Integer> result=new ArrayList<>();
+        // Time Complexity: O(n)
+        //  Space Complexity: O(n)
 
-        int ans=1;
+        List<Integer> result = new ArrayList<>();
+
+        int ans = 1;
         result.add(ans);
 
-        for(int i=1;i<n;i++){
+        for (int i = 1; i < n; i++) {
 
-            ans=ans*(n-i);
-            ans=ans/i;
+            ans = ans * (n - i);
+            ans = ans / i;
             result.add(ans);
 
 
-
         }
 
         return result;
     }
 
-    static List<List<Integer>> pascalTriangleIII(int n){
+    static List<List<Integer>> pascalTriangleIII(int n) {
 
         //Type 3-> you are given n  and  u have  to print the entire pascal triangle
 
-        List<List<Integer>> result=new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
 
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
 
-            List<Integer> tempList=new ArrayList<>();
+            List<Integer> tempList = new ArrayList<>();
 
-            for(int j=0;j<i+1;j++){
+            for (int j = 0; j < i + 1; j++) {
 
-                if(j==0 || j==i){
+                if (j == 0 || j == i) {
                     tempList.add(1);
-                }
-
-                else {
-                    int val=result.get(i-1).get(j-1)+result.get(i-1).get(j);
+                } else {
+                    int val = result.get(i - 1).get(j - 1) + result.get(i - 1).get(j);
                     tempList.add(val);
                 }
 
@@ -110,26 +109,19 @@ public class Q23 {
         return result;
 
 
+    }
 
 
-        }
-
-
-
-
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
         // pascal triangle  leeTCode->118
 
 
-
-        System.out.println(pascalTriangleI(5,3));
-        List<Integer> ans=pascalTriangleII(3);
+        System.out.println(pascalTriangleI(5, 3));
+        List<Integer> ans = pascalTriangleII(3);
         System.out.println(ans);
-        List<List<Integer>> answer=pascalTriangleIII(5);
+        List<List<Integer>> answer = pascalTriangleIII(5);
         System.out.println(answer);
-
-
 
 
     }
--- src/myPractice.java
+++ src/myPractice.java
@@ -1,81 +1,27 @@
-import OopsByKK.AccessModifier.Public.A;
-import OopsByKK.AccessModifier.Public.B;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class myPractice {
 
-    static void swap(int[][] arr, int i, int j) {
-
-        int temp = arr[i][j];
-        arr[i][j] = arr[j][i];
-        arr[j][i] = temp;
-    }
-
 
     static int[][] func(int n) {
 
+        int[][] result = new int[n][];
 
-        int[][] result = new int[n][n];
-
-        int TopRow = 0;
-        int BottomRow = n - 1;
-        int LeftCol = 0;
-        int RightCol = n - 1;
-        int num=1;
+        for (int i = 1; i < n; i++) {
 
 
-        while (TopRow <= BottomRow && LeftCol <= RightCol) {
+            result[i] = new int[i + 1];
+            result[i][0] = result[i][i] = 1;
 
+            for (int j=1;j<i+1;j++){
 
-
-            for (int i = LeftCol; i <= RightCol; i++) {
-
-              result[TopRow][i]=num++;
-
-            }
-
-            TopRow++;
-
-
-            for (int i = TopRow; i <= BottomRow; i++) {
-
-                result[i][RightCol]=num++;
-
-            }
-
-            RightCol--;
-
-            if (TopRow <= BottomRow && LeftCol <= RightCol) {
-
-                for (int i = RightCol; i >= LeftCol; i--) {
-
-                    result[BottomRow][i]=num++;
-                }
-
-                BottomRow--;
-
-
-            }
-
-
-            if (TopRow <= BottomRow && LeftCol <= RightCol) {
-
-                for (int i = BottomRow; i >= TopRow; i--) {
-
-                    result[i][LeftCol]=num++;
-                }
-
-                LeftCol++;
 
             }
 
 
         }
-
-        return result;
 
 
     }
@@ -83,12 +29,10 @@ public class myPractice {
 
     public static void main(String[] args) {
 
-
-        int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};  // square matrix
-
-
-        int[][] ans = func(3);
-        System.out.println(Arrays.deepToString(ans));
+        int r = 5;
+        int c = 3;
+        List<Integer> ans = func(6);
+        System.out.println(ans);
 
 
     }
