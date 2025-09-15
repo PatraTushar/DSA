5b502a408842ecb0dcbe5612d142305c70293f29
Tushar Patra <tushar04012002@gmail.com>
2025-09-15 21:50:14 +0530

    dsa

---
+++ src/ArraysbyStriver/binarySearchPattern/Q29.java
@@ -0,0 +1,63 @@
+package ArraysbyStriver.binarySearchPattern;
+
+public class Q29 {
+
+
+    // BRUTE FORCE APPROACH
+    static int findKthPositive(int[] arr, int k) {
+
+        // Time Complexity: O( n)
+        // Space Complexity: O(1)
+
+        for (int ele : arr) {
+
+            if (ele <= k) k++;
+
+            else break;
+        }
+
+        return k;
+
+    }
+
+
+    static int findKthPositiveI(int[] arr, int k) {
+
+        // Time Complexity: O(log n)
+        // Space Complexity: O(1)
+
+        int n = arr.length;
+
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            int missing = arr[mid] - (mid + 1);
+
+            if (missing < k) {
+
+                low = mid + 1;
+
+
+            } else high = mid - 1;
+        }
+
+
+        return (high + 1 + k);
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 3, 4, 7, 11};
+        int k = 5;
+        System.out.println(findKthPositiveI(arr, k));
+
+
+    }
+}
--- src/myPractice.java
+++ src/myPractice.java
@@ -1,62 +1,7 @@
 public class myPractice {
 
-    static int findMax(int[] arr) {
-
-        int max = Integer.MIN_VALUE;
-
-        for (int ele : arr) {
-
-            if (ele > max) {
-
-                max = ele;
-            }
-
-        }
-
-        return max;
-    }
-
-
-    static int calculateHours(int[] piles, int mid) {
-
-        int totalHrs = 0;
-
-        for (int ele : piles) {
-
-            totalHrs += Math.ceil((double) ele / mid);
-        }
-
-        return totalHrs;
-    }
-
-
-    static int minEatingSpeed(int[] piles, int hrs) {
-
-        int start = 1;
-        int end = findMax(piles);
-
-        while (start <= end) {
-
-            int mid = start + (end - start) / 2;
-
-            int hrsTaken = calculateHours(piles, mid);
-
-            if (hrsTaken > hrs) {
-
-                start = mid + 1;
-            } else end = mid - 1;
-
-
-        }
-
-        return start;
-    }
-
     public static void main(String[] args) {
 
-        int[] piles = {3, 6, 7, 11};
-        int hrs = 8;
-        System.out.println(minEatingSpeed(piles, hrs));
 
     }
 }
