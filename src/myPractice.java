5bbd33cbdb4da7e0d896784d5ec819046062854c
Tushar Patra <tushar04012002@gmail.com>
2025-09-15 13:29:38 +0530

    dsa

--- src/myPractice.java
+++ src/myPractice.java
@@ -1,35 +1,6 @@
 public class myPractice {
 
-
-    static int findSqrRoot(int x) {
-
-        int start = 1;
-        int end = x;
-
-        while (start <= end) {
-
-            int mid = start + (end - start) / 2;
-            int sqrMid = mid * mid;
-
-            if (sqrMid == x) return mid;
-
-            else if (sqrMid > x) end = mid - 1;
-
-            else start = mid + 1;
-
-        }
-
-        return end;
-
-    }
-
-
     public static void main(String[] args) {
-
-
-        int x = 4;
-        System.out.println(findSqrRoot(x));
-
 
     }
 }
