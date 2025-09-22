f548f5dc0d25d1bfa89266358e126e4baadf31ea
Tushar Patra <tushar04012002@gmail.com>
2025-09-22 22:55:43 +0530

    dsa

--- src/ArraysbyStriver/strivers/Q24.java
+++ src/ArraysbyStriver/strivers/Q24.java
@@ -79,70 +79,6 @@ public class Q24 {
         System.out.println(Arrays.toString(arr1));
     }
 
-    static void swapIfGreater(int[] arr1, int[] arr2, int i, int j) {
-
-        if (arr1[i] > arr2[j]) {
-            int temp = arr1[i];
-            arr1[i] = arr2[j];
-            arr2[j] = temp;
-        }
-    }
-
-    static void merge(int[] arr1, int[] arr2, int m, int n) {
-
-        //TC = O((m + n) * log(m + n))
-        //SC = O(1)
-
-
-        //🧠 Purpose of Your Code
-        //You are:
-        //Merging two sorted arrays arr1[] of size m and arr2[] of size n
-        //Using the Gap Method (Shell Sort concept) to do this in-place (i.e., without using extra space)
-        //Ensuring the final result is sorted across both arrays
-
-        int length = m + n;
-        int gap = (length / 2) + (length % 2);
-
-
-        while (gap > 0) {
-
-            int left = 0;
-            int right = gap;
-
-            while (right < length) {
-
-
-                // Case 1: both pointers in arr1
-                if (left < m && right < m) {
-                    swapIfGreater(arr1, arr1, left, right);
-                }
-
-                // Case 2: left in arr1, right in arr2
-                else if (left < m && right >= m) {
-                    swapIfGreater(arr1, arr2, left, right - m);
-                }
-
-                // Case 3: both pointers in arr2
-                else {
-                    swapIfGreater(arr2, arr2, left - m, right - m);
-                }
-
-                left++;
-                right++;
-            }
-
-            if (gap == 1) break;
-            gap = (gap / 2) + (gap % 2);
-
-
-        }
-
-        System.out.print(Arrays.toString(arr1));
-        System.out.println(Arrays.toString(arr2));
-
-
-    }
-
 
     public static void main(String[] args) {
 
@@ -151,7 +87,7 @@ public class Q24 {
         int m = 3;
         int[] arr2 = {2, 5, 6};
         int n = 3;
-        merge(arr1, arr2, m, n);
+        // mergeI(arr1,m,arr2,n);
         mergeII(arr1, m, arr2, n);
 
 
--- src/myPractice.java
+++ src/myPractice.java
@@ -2,78 +2,7 @@ import java.util.*;
 
 public class myPractice {
 
-    static void swap(int[] arr1, int[] arr2, int left, int right) {
-
-        if (arr1[left] > arr2[right]) {
-
-            int temp = arr1[left];
-            arr1[left] = arr2[right];
-            arr2[right] = temp;
-        }
-
-
-    }
-
-    static void merge(int[] a, int[] b) {
-
-        int m = a.length;
-        int n = b.length;
-
-        int length = m + n;
-        int gap = length / 2 + length % 2;
-
-
-        while (gap >0) {
-
-            int left = 0;
-            int right = gap;
-
-
-            while (right < length) {
-
-                // one pointer is in arr1 and one pointer is in arr2
-
-                if (left < m && right < m) {
-
-                    swap(a, a, left, right);
-
-
-                } else if (left < m && right >= m) {
-
-                    swap(a, b, left, right - m);
-
-
-                } else {
-
-                    swap(b, b, right - m, right - m);
-
-
-                }
-
-                left++;
-                right++;
-
-
-            }
-
-            if (gap==1 ) break;
-            gap = gap / 2 + gap % 2;
-
-        }
-
-        System.out.println(Arrays.toString(a));
-        System.out.println(Arrays.toString(b));
-
-
-    }
-
-
     public static void main(String[] args) {
-
-        int[] arr1 = {2, 4, 7, 10};
-        int[] arr2 = {2, 3};
-        merge(arr1, arr2);
-
 
 
     }
