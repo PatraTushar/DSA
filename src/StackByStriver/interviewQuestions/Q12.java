776194a9a1e3d426c191ccfca50b388eb25ea014
Tushar Patra <tushar04012002@gmail.com>
2025-10-17 23:12:14 +0530

    dsa

--- src/StackByStriver/interviewQuestions/Q12.java
+++ src/StackByStriver/interviewQuestions/Q12.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 
 public class Q12 {
 
-    static int[] asteroidCollision(int[] asteroids){
+    static int[] asteroidCollision(int[] asteroids) {
 
         //Time Complexity (TC): O(N)
         //Space Complexity (SC): O(N)
@@ -13,46 +13,41 @@ public class Q12 {
 
         if (asteroids == null || asteroids.length == 0) return new int[0];
 
-        Stack<Integer> st=new Stack<>();
+        Stack<Integer> st = new Stack<>();
 
-        for(int ele : asteroids){
-
-
-
-            boolean destroyed=false;
+        for (int ele : asteroids) {
 
 
-            while (!st.isEmpty() && ele<0 && st.peek()>0){
+            boolean destroyed = false;
 
-                if(Math.abs(ele)>st.peek()){
+
+            while (!st.isEmpty() && ele < 0 && st.peek() > 0) {
+
+                if (Math.abs(ele) > st.peek()) {
                     st.pop();
                     continue;
-                }
-
-                else if(Math.abs(ele)<st.peek()){
-                    destroyed=true;
+                } else if (Math.abs(ele) < st.peek()) {
+                    destroyed = true;
                     break;
-                }
-
-                else {
+                } else {
                     st.pop();
-                    destroyed=true;
+                    destroyed = true;
                     break;
                 }
 
 
             }
 
-            if(!destroyed){
+            if (!destroyed) {
 
                 st.push(ele);
             }
         }
 
-        int n=st.size();
-        int[] result =new int[n];
-        for(int i=n-1;i>=0;i--){
-            result[i]=st.pop();
+        int n = st.size();
+        int[] result = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] = st.pop();
         }
 
         return result;
@@ -60,8 +55,8 @@ public class Q12 {
 
     public static void main(String[] args) {
 
-        int[] arr={4,7,1,1,2,-3,-7,17,15,-16};
-        int[] ans=asteroidCollision(arr);
+        int[] arr = {4, 7, 1, 1, 2, -3, -7, 17, 15, -16};
+        int[] ans = asteroidCollision(arr);
         System.out.println(Arrays.toString(ans));
     }
 }
--- src/StackByStriver/interviewQuestions/Q17.java
+++ src/StackByStriver/interviewQuestions/Q17.java
@@ -4,43 +4,43 @@ import java.util.Arrays;
 
 public class Q17 {
 
-    static int[] slidingWindow(int[] arr,int k){
+    static int[] slidingWindow(int[] arr, int k) {
 
         // brute Force Approach
 
         // Time Complexity (TC): O(n * k)
         //Space Complexity (SC): O(n - k + 1)
 
-        int n=arr.length;
-        int[] sWindow=new int[n-k+1];
+        int n = arr.length;
+        int[] sWindow = new int[n - k + 1];
 
-        for(int i=0;i<=n-k;i++){
+        for (int i = 0; i <= n - k; i++) {
 
-            int maxInWindow=arr[i];
+            int maxInWindow = arr[i];
 
-            for(int j=i;j<i+k;j++){
+            for (int j = i; j < i + k; j++) {
 
-                maxInWindow=Math.max(maxInWindow,arr[j]);
+                maxInWindow = Math.max(maxInWindow, arr[j]);
 
             }
 
-            sWindow[i]=maxInWindow;
+            sWindow[i] = maxInWindow;
         }
 
         return sWindow;
     }
 
 
-    static void slidingWindowI(int[] num,int k){
+    static void slidingWindowI(int[] num, int k) {
 
 
     }
 
     public static void main(String[] args) {
 
-        int[] arr={1,3,-1,-3,5,3,2,1,6};
-        int k=3;
-        int[] ans=slidingWindow(arr,k);
+        int[] arr = {1, 3, -1, -3, 5, 3, 2, 1, 6};
+        int k = 3;
+        int[] ans = slidingWindow(arr, k);
         System.out.println(Arrays.toString(ans));
 
     }
