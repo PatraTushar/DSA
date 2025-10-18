5dfebbb3a22fd20a571b9f25360d0984cbccb3fb
Tushar Patra <tushar04012002@gmail.com>
2025-10-18 23:21:25 +0530

    dsa

---
+++ src/StackByStriver/interviewQuestions/Q20.java
@@ -0,0 +1,58 @@
+package StackByStriver.interviewQuestions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Q20 {
+
+    static ArrayList<Integer> arrayBeautiful(int[] arr) {
+
+        // Time Complexity (TC): O(n)
+        //Space Complexity (SC): O(n)
+
+
+        Stack<Integer> st = new Stack<>();
+
+
+        for (int ele : arr) {
+
+            if (st.isEmpty()) st.push(ele);
+
+            else if (ele >= 0) {
+
+                if (st.peek() >= 0) st.push(ele);
+                else st.pop();
+
+
+            } else {
+
+                if (st.peek() < 0) st.push(ele);
+                else st.pop();
+
+
+            }
+
+
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+
+        for (int ele : st) {
+
+            result.add(ele);
+        }
+
+        return result;
+
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {4, 2, -2, 1};
+        ArrayList<Integer> ans = arrayBeautiful(arr);
+        System.out.println(ans);
+    }
+}
