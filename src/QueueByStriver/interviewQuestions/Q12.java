64d01719b3993d7c2b5cd8397603ba6ec4013eef
Tushar Patra <tushar04012002@gmail.com>
2025-10-22 02:54:12 +0530

    dsa

--- src/QueueByStriver/interviewQuestions/Q12.java
+++ src/QueueByStriver/interviewQuestions/Q12.java
@@ -7,13 +7,11 @@ public class Q12 {
     static int firstNegative(Queue<Integer> q, int k) {
 
 
-
-
         int val = 0;
 
         while (k > 0) {
 
-            if ( !q.isEmpty() && q.peek() < 0) {
+            if (!q.isEmpty() && q.peek() < 0) {
 
                 val = q.peek();
                 break;
@@ -74,12 +72,52 @@ public class Q12 {
 
     }
 
+
+    static List<Integer> firstNegativeInWindowKI(int[] arr, int k) {
+
+        // Time Complexity: O(n)
+        //Space Complexity: O(k) + O(n - k + 1) ≈ O(n)
+
+
+        int n = arr.length;
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < k - 1; i++) {
+
+            if (arr[i] < 0) q.add(i);
+
+
+        }
+
+
+        for (int i = k - 1; i < n; i++) {
+
+            if (arr[i] < 0) q.add(i);
+
+
+            while (!q.isEmpty() && q.peek() <= i - k) q.poll();
+
+
+            if (q.isEmpty()) result.add(0);
+            else result.add(arr[q.peek()]);
+
+
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
 
         int[] arr = {2, -3, -4, -2, 7, 8, 9, -10};
+        int[] arr1 = {2, -3, -4, -2, 7, 8, 9, -10};
         int k = 3;
-        List<Integer> ans=firstNegativeInWindowK(arr,k);
+        int k1 = 3;
+        List<Integer> ans = firstNegativeInWindowK(arr, k);
         System.out.println(ans);
+        List<Integer> ans1 = firstNegativeInWindowKI(arr1, k1);
+        System.out.println(ans1);
 
     }
 }
