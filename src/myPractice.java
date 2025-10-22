1a8ea5f231907ef5eee22c21a6ad035bcaaaaf1a
Tushar Patra <tushar04012002@gmail.com>
2025-10-22 15:28:06 +0530

    dsa

--- src/myPractice.java
+++ src/myPractice.java
@@ -1,24 +1,27 @@
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class myPractice {
 
-    static void print(Queue<Integer> q){
-
-
-    }
 
 
     public static void main(String[] args) {
 
-        Queue<Integer> q=new LinkedList<>();
+        Deque<Integer> q=new LinkedList<>();
         q.add(1);
         q.add(2);
         q.add(3);
         q.add(4);
         q.add(5);
 
-        System.out.println(q.peek());
+        System.out.println(q);
+
+        q.offerFirst(344);
+        System.out.println(q);
+
+
+
 
 
 
