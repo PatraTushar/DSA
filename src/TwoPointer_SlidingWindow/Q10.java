4a9d5d9e2b2bbf6deda600512792dc67bfa60030
Tushar Patra <tushar04012002@gmail.com>
2025-10-29 10:14:44 +0530

    dsa

--- src/TwoPointer_SlidingWindow/Q10.java
+++ src/TwoPointer_SlidingWindow/Q10.java
@@ -8,70 +8,129 @@ public class Q10 {
         //  Space Complexity: O(26)=O(1)
 
         int n = s.length();
-        int maxLength = 0;
-
+        int longest = 0;
 
         for (int i = 0; i < n; i++) {
 
             int[] freq = new int[26];
             int maxFreq = 0;
 
-
             for (int j = i; j < n; j++) {
 
-                freq[s.charAt(j) - 'A']++;
+                int index = s.charAt(j) - 'A';
+                freq[index]++;
+                maxFreq = Math.max(maxFreq, freq[index]);
 
-                maxFreq = Math.max(maxFreq, freq[s.charAt(j) - 'A']);
                 int changes = (j - i + 1) - maxFreq;
 
                 if (changes <= k) {
-                    maxLength = Math.max(maxLength, j - i + 1);
+
+                    longest = Math.max(longest, j - i + 1);
+
                 } else break;
 
 
             }
+
         }
 
-        return maxLength;
+        return longest;
+
 
     }
 
 
+    // BETTER APPROACH
     static int characterReplacementI(String s, int k) {
+
+        int n = s.length();
+        int longest = 0;
+        int[] freq = new int[26];
+        int left = 0;
+        int maxFreq = 0;
+
+        for (int right = 0; right < n; right++) {
+
+            int rightIndex = s.charAt(right) - 'A';
+            freq[rightIndex]++;
+            maxFreq = Math.max(maxFreq, freq[rightIndex]);
+
+            int changes = (right - left + 1) - maxFreq;
+
+            if (changes <= k) {
+
+                longest = Math.max(longest, right - left + 1);
+
+            } else {
+
+
+                while (changes > k) {
+
+                    int leftIndex = s.charAt(left) - 'A';
+                    freq[leftIndex]--;
+                    left++;
+
+                    int newMax = 0;
+                    for (int t = 0; t < 26; t++) {
+                        newMax = Math.max(newMax, freq[t]);
+                    }
+                    maxFreq = newMax;
+
+                    changes = (right - left + 1) - maxFreq;
+
+                }
+
+                longest = Math.max(longest, right - left + 1);
+
+
+            }
+
+
+        }
+
+        return longest;
+    }
+
+
+    // OPTIMAL APPROACH
+    static int characterReplacementII(String s, int k) {
 
         //  Time Complexity: O(n)
         //  Space Complexity: O(26)=O(1)
 
 
         int n = s.length();
+        int longest = 0;
         int[] freq = new int[26];
-        int maxFreq = 0;
-        int maxLength = 0;
         int left = 0;
-        int right = 0;
+        int maxFreq = 0;
 
-        for (int i = 0; i < n; i++) {
+        for (int right = 0; right < n; right++) {
 
-            freq[s.charAt(right) - 'A']++;
+            int rightIndex = s.charAt(right) - 'A';
+            freq[rightIndex]++;
+            maxFreq = Math.max(maxFreq, freq[rightIndex]);
 
-            maxFreq = Math.max(maxFreq, freq[s.charAt(right) - 'A']);
+            int changes = (right - left + 1) - maxFreq;
 
+            if (changes > k) {
 
-            while ((right - left + 1) - maxFreq > k) {
-
-                freq[s.charAt(left) - 'A']--;
+                int leftIndex = s.charAt(left) - 'A';
+                freq[leftIndex]--;
                 left++;
+
+
             }
 
-            maxLength = Math.max(maxLength, right - left + 1);
+            longest = Math.max(longest, right - left + 1);
 
 
-            right++;
         }
 
-        return maxLength;
+        return longest;
 
     }
+
 
     public static void main(String[] args) {
 
--- src/myPractice.java
+++ src/myPractice.java
@@ -5,10 +5,42 @@ public class myPractice {
 
     static int func(String s, int k) {
 
+        int n = s.length();
+        int longest = 0;
+        int[] freq = new int[26];
+        int left = 0;
+        int maxFreq = 0;
+
+        for (int right = 0; right < n; right++) {
+
+            int rightIndex = s.charAt(right) - 'A';
+            freq[rightIndex]++;
+            maxFreq = Math.max(maxFreq, freq[rightIndex]);
+
+            int changes = (right - left + 1) - maxFreq;
+
+            if (changes > k) {
+
+                int leftIndex = s.charAt(left) - 'A';
+                freq[leftIndex]--;
+                left++;
+
+
+            }
+
+            longest = Math.max(longest, right - left + 1);
+
+
+        }
+
+        return longest;
 
     }
 
     public static void main(String[] args) {
+
+        String s = "AABABBA";
+        System.out.println(func(s, 2));
 
 
     }
