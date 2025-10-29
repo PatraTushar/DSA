46ee039319d8b907e9669a83ec2c5fede09faf77
Tushar Patra <tushar04012002@gmail.com>
2025-10-29 20:50:23 +0530

    dsa

--- src/TwoPointer_SlidingWindow/Q12.java
+++ src/TwoPointer_SlidingWindow/Q12.java
@@ -2,6 +2,37 @@ package TwoPointer_SlidingWindow;
 
 public class Q12 {
 
+    static int numberOfSubArrays(int[] arr, int k) {
+
+        //  Time Complexity: O(n^2)
+        //  Space Complexity: O(1)
+
+
+        int n = arr.length;
+        int totalSubArray = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            int count = 0;
+
+            for (int j = i; j < n; j++) {
+
+                if (arr[j] % 2 != 0) count++;
+
+                if (k == count) {
+
+                    totalSubArray++;
+
+
+                } else if (count > k) break;
+
+
+            }
+        }
+
+        return totalSubArray;
+    }
+
 
     static int subArraysWithSumLessThanEqualToK(int[] arr, int k) {
 
@@ -14,12 +45,12 @@ public class Q12 {
 
         for (int right = 0; right < n; right++) {
 
-            sum += arr[right]%2;
+            sum += arr[right] % 2;
 
 
             while (sum > k) {
 
-                sum -= arr[left]%2;
+                sum -= arr[left] % 2;
                 left++;
             }
 
@@ -31,7 +62,7 @@ public class Q12 {
         return totalNiceSubArray;
     }
 
-    static int numberOfSubArrays(int[] arr, int k) {
+    static int numberOfSubArraysI(int[] arr, int k) {
 
         //  Time Complexity: O(n)
         //  Space Complexity: O(1)
@@ -44,9 +75,10 @@ public class Q12 {
 
         // leeTCode->1248
 
-        int[] arr={1,1,2,1,1};
-        int k=3;
-        System.out.println(numberOfSubArrays(arr,k));
+        int[] arr = {1, 1, 2, 1, 1};
+        int k = 3;
+        System.out.println(numberOfSubArrays(arr, k));
+        System.out.println(numberOfSubArraysI(arr, k));
 
     }
 }
--- src/myPractice.java
+++ src/myPractice.java
@@ -1,36 +1,40 @@
-import java.util.HashMap;
-import java.util.Stack;
-
 public class myPractice {
 
-    static int func(int[] arr, int goal) {
+    static int func(int[] arr, int k) {
 
         int n = arr.length;
-        int count = 0;
+        int totalSubArray = 0;
 
         for (int i = 0; i < n; i++) {
 
-            int sum = 0;
+            int count=0;
 
             for (int j = i; j < n; j++) {
 
-                sum += arr[j];
+                if (arr[j] % 2 != 0 ) count++;
 
-                if (sum == goal) count++;
+                if (k == count) {
 
-                if (sum > goal) break;
+                    totalSubArray++;
+
+
+                }
+
+                else if(count > k) break;
+
+
 
 
             }
         }
 
-        return count;
-
+        return totalSubArray;
     }
 
+
     public static void main(String[] args) {
-        int[] arr = {1, 0, 1, 0, 1};
-        int goal = 2;
+        int[] arr = {1,2,4};
+        int goal = 1;
         System.out.println(func(arr, goal));
 
 
