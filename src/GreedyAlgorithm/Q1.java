7bb86574737937a85849560ecb08db4bacbde297
Tushar Patra <tushar04012002@gmail.com>
2025-10-30 14:30:30 +0530

    dsa

---
+++ src/GreedyAlgorithm/Q1.java
@@ -0,0 +1,44 @@
+package GreedyAlgorithm;
+
+import java.util.Arrays;
+
+public class Q1 {
+
+
+    static int findContentChildren(int[] g, int[] s) {
+
+        //  Time Complexity: O(n log n) + O(m log m) + O(n)
+        //  Space Complexity: O(1)
+
+
+
+        int n = s.length;
+        int m = g.length;
+
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int left = 0;
+        int right = 0;
+
+        while (left < n && right < m) {
+
+
+            if (s[left] >= g[right]) right++;
+
+            left++;
+
+        }
+
+        return right;
+
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] greed = {1, 5, 3, 3, 4};
+        int[] s = {4, 2, 1, 2, 1, 3};
+        System.out.println(findContentChildren(greed, s));
+    }
+}
--- src/myPractice.java
+++ src/myPractice.java
@@ -1,71 +1,14 @@
-import java.util.HashMap;
+
 
 public class myPractice {
 
 
-    static String func(String s, String t) {
+    static void func(String s, String t) {
 
-        int m = s.length();
-        int n = t.length();
-        int minLength = Integer.MAX_VALUE;
-        int startingIndex = -1;
-        HashMap<Character, Integer> map = new HashMap<>();
-        int left = 0;
-        int count = 0;
-
-        for (int i = 0; i < n; i++) {
-
-            char ch = t.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }
-
-
-        for (int right = 0; right < m; right++) {
-
-            char ch = s.charAt(right);
-
-            if (map.containsKey(ch) && map.get(ch) > 0) count++;
-
-            if (map.containsKey(ch)) map.put(ch, map.get(ch) - 1);
-
-            while (count == n) {
-
-                int length = right - left + 1;
-
-                if (length < minLength) {
-
-                    minLength = length;
-                    startingIndex = left;
-                }
-
-
-                if (map.containsKey(s.charAt(left))) {
-                    map.put(s.charAt(left), map.get(s.charAt(left)) + 1);
-
-                    if (map.get(s.charAt(left)) > 0) count--;
-
-                }
-
-
-                left++;
-
-
-            }
-
-
-        }
-
-        return minLength == Integer.MAX_VALUE ? "" : s.substring(startingIndex, minLength + startingIndex);
 
     }
 
     public static void main(String[] args) {
-        String s = "ADOBECODEBANC";
-        String s1 = "ABCDE";
-        String t = "ABC";
-        String t1 = "AB";
-        System.out.println(func(s, t));
-
 
     }
 
