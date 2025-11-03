f8a1d0983a4740378caaaaf947e0ae47c4eb802d
Tushar Patra <tushar04012002@gmail.com>
2025-11-03 21:21:11 +0530

    dsa

---
+++ src/Recursion/CodeWithMik/Q3.java
@@ -0,0 +1,59 @@
+package Recursion.CodeWithMik;
+
+import java.util.ArrayList;
+
+public class Q3 {
+
+    static int[] dRow = {0, 1, 0, -1};
+    static int[] dCols = {-1, 0, 1, 0};
+    static char[] direction = {'L', 'D', 'R', 'U'};
+
+    static void solve(int[][] maze, int row, int cols, String path, ArrayList<String> result, int n) {
+
+        if (row < 0 || cols < 0 || row >= n || cols >= n || maze[row][cols] == 0) return;
+
+        if (row == n - 1 && cols == n - 1) {
+
+            result.add(path);
+            return;
+
+        }
+
+
+        maze[row][cols] = 0;
+
+        for (int i = 0; i < 4; i++) {
+
+
+            int nextRow = row + dRow[i];
+            int nextCols = cols + dCols[i];
+            solve(maze, nextRow, nextCols, path + direction[i], result, n);
+
+
+        }
+
+        maze[row][cols] = 1;
+
+
+    }
+
+    static ArrayList<String> rateInMaze(int[][] maze) {
+
+        int n = maze.length;
+
+        ArrayList<String> possiblePath = new ArrayList<>();
+
+        if (maze[0][0] == 1) solve(maze, 0, 0, "", possiblePath, n);
+
+        return possiblePath;
+
+
+    }
+
+    public static void main(String[] args) {
+
+        int[][] maze = {{1, 0, 0, 0}, {1, 1, 0, 1}, {1, 1, 0, 0}, {0, 1, 1, 1}};
+        System.out.println(rateInMaze(maze));
+
+    }
+}
