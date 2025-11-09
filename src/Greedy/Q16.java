6deec0b6396e0ff806e86ca051f90dc6055b330b
Tushar Patra <tushar04012002@gmail.com>
2025-11-09 14:32:27 +0530

    dsa

--- src/Greedy/Q16.java
+++ src/Greedy/Q16.java
@@ -2,6 +2,42 @@ package Greedy;
 
 public class Q16 {
 
+    static int minTaps(int n, int[] ranges) {
+
+        int[] maxReach = new int[n + 1];
+
+        // Step 1: Build maxReach array
+        for (int i = 0; i <= n; i++) {
+
+            int left = Math.max(0, i - ranges[i]);
+            int right = Math.min(n, i + ranges[i]);
+
+            maxReach[left] = Math.max(maxReach[left], right);
+        }
+
+        // Step 2: Greedy sweep
+        int openTaps = 0;
+        int currEnd = 0;
+        int nextEnd = 0;
+
+        for (int i = 0; i <= n; i++) {
+
+            if (i > nextEnd) return -1;  // cannot reach this point
+
+            nextEnd = Math.max(nextEnd, maxReach[i]);
+
+            if (i == currEnd) {
+
+                if (currEnd != nextEnd) {
+                    openTaps++;
+                    currEnd = nextEnd;
+                }
+            }
+        }
+
+        return openTaps;
+    }
+
     public static void main(String[] args) {
 
         // leeTCode->1326
---
+++ src/Greedy/Q17.java
@@ -0,0 +1,54 @@
+package Greedy;
+
+
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Q17 {
+
+    static int minDeletions(String s) {
+
+        //  Time Complexity: O(n)
+        //  Space Complexity: O(26)->O(1)
+
+        int n = s.length();
+        HashMap<Character, Integer> map = new HashMap<>();
+
+
+        for (int i = 0; i < n; i++) {
+
+            char ch = s.charAt(i);
+
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+        int deletion = 0;
+
+
+        for (char key : map.keySet()) {
+
+            int freq = map.get(key);
+
+            while (freq > 0 && set.contains(freq)) {
+
+                freq--;
+                deletion++;
+
+            }
+
+            if (freq > 0) set.add(freq);
+        }
+
+        return deletion;
+
+
+    }
+
+    public static void main(String[] args) {
+
+        String s = "abcdabcd";
+        System.out.println(minDeletions(s));
+    }
+}
