22f3e70f821f122b9891f1a81d2a9abeafc8480b
Tushar Patra <tushar04012002@gmail.com>
2025-11-14 22:56:55 +0530

    dsa

--- src/Greedy/Q33.java
+++ src/Greedy/Q33.java
@@ -2,33 +2,33 @@ package Greedy;
 
 public class Q33 {
 
-    static long maximumValueSum(int[] nums, int k, int[][] edges) {
+    static long maximumValueSum(int[] arr, int k, int[][] edges) {
 
         //  Time Complexity: O(n)
         //  Space Complexity: O(1)
 
 
-        int n = nums.length;
+        int n = arr.length;
         long result = 0;
         int xorApplied = 0;
         int loss = Integer.MAX_VALUE;
 
         for (int i = 0; i < n; i++) {
 
-            int XOR = nums[i] ^ k;
+            int XOR = arr[i] ^ k;
 
 
-            if (XOR > nums[i]) {
+            if (XOR > arr[i]) {
 
                 result += XOR;
                 xorApplied++;
             } else {
 
-                result += nums[i];
+                result += arr[i];
 
             }
 
-            loss = Math.min(loss, Math.abs(nums[i] - XOR));
+            loss = Math.min(loss, Math.abs(arr[i] - XOR));
 
 
         }
@@ -46,10 +46,10 @@ public class Q33 {
 
         // leeTCode->3068
 
-        int[] nums = {1, 2, 1};
+        int[] arr = {1, 2, 1};
         int k = 3;
         int[][] edges = {{0, 1}, {0, 2}};
-        System.out.println(maximumValueSum(nums, k, edges));
+        System.out.println(maximumValueSum(arr, k, edges));
 
     }
 }
---
+++ src/Greedy/Q34.java
@@ -0,0 +1,9 @@
+package Greedy;
+
+public class Q34 {
+
+    public static void main(String[] args) {
+
+
+    }
+}
