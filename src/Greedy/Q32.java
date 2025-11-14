cf227cf25de0f99ea5e72fd708ae129d74975c1e
Tushar Patra <tushar04012002@gmail.com>
2025-11-14 20:32:11 +0530

    dsa

---
+++ src/Greedy/Q32.java
@@ -0,0 +1,45 @@
+package Greedy;
+
+import java.util.Arrays;
+
+public class Q32 {
+
+    static long maximumHappinessSum(int[] happiness, int k) {
+
+        //  Time Complexity: O(n log n) + O(n)=O(n log n)
+        //  Space Complexity: O(1)
+
+
+        int n = happiness.length;
+
+        Arrays.sort(happiness);
+
+
+        int i = n - 1;
+        int decrement = 0;
+        long selectedChildren = 0;
+
+        while (i >= 0 && k > 0) {
+
+
+            if (happiness[i] - decrement < 0) selectedChildren += 0;
+
+            else selectedChildren += happiness[i] - decrement;
+
+            decrement++;
+            i--;
+            k--;
+
+
+        }
+
+        return selectedChildren;
+    }
+
+    public static void main(String[] args) {
+
+        int[] happiness = {1, 2, 3};
+        int k = 2;
+        System.out.println(maximumHappinessSum(happiness, k));
+    }
+}
