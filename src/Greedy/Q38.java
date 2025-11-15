0b80edb09b113aa000d05b37288a6721fc5748a5
Tushar Patra <tushar04012002@gmail.com>
2025-11-15 21:27:56 +0530

    dsa

---
+++ src/Greedy/Q38.java
@@ -0,0 +1,48 @@
+package Greedy;
+
+import java.util.HashMap;
+
+public class Q38 {
+
+    static int maxCount(int[] banned, int n, int maxSum) {
+
+        //  Time Complexity: O( length + n)
+        //  Space Complexity: O(length)
+
+        int length = banned.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < length; i++) {
+
+            map.put(banned[i], map.getOrDefault(banned[i], 0) + 1);
+        }
+
+        int maxNumber = 0;
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+
+            if (!map.containsKey(i) && sum + i <= maxSum) {
+
+                maxNumber++;
+                sum += i;
+            }
+        }
+
+        return maxNumber;
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        // leeTCode->2554
+
+        int[] banned = {1, 6, 5};
+        int n = 5;
+        int maxSum = 6;
+        System.out.println(maxCount(banned, n, maxSum));
+
+    }
+}
