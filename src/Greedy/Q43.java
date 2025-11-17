cb2b5b82584856b8f7b11f8b16450c3dcb443aae
Tushar Patra <tushar04012002@gmail.com>
2025-11-18 01:00:12 +0530

    dsa

---
+++ src/Greedy/Q43.java
@@ -0,0 +1,21 @@
+//package Greedy;
+//
+//public class Q43 {
+//
+//    static int maxDistance(String s, int k) {
+//
+//
+//
+//
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        // leeTCode->3443
+//
+//        String s = "NSWWEW";
+//        int k = 3;
+//        System.out.println(maxDistance(s, k));
+//
+//    }
+//}
---
+++ src/Greedy/Q44.java
@@ -0,0 +1,40 @@
+package Greedy;
+
+public class Q44 {
+
+    static int longestSubsequence(String s, int k) {
+
+        //  Time Complexity: O(n)
+        //  Space Complexity: O(1)
+
+
+        int n = s.length();
+        int length = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            int number = s.charAt(i) - '0';
+            int val = number * (int) Math.pow(2, n - i - 1);
+
+            if (val > k) continue;
+
+            k -= val;
+            length++;
+
+
+        }
+
+        return length;
+
+
+    }
+
+    public static void main(String[] args) {
+
+        String s = "1001010";
+        int k = 5;
+        System.out.println(longestSubsequence(s, k));
+
+
+    }
+}
