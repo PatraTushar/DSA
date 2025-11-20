d9c8300376786e374ec880f8471c3b1232a0ba29
Tushar Patra <tushar04012002@gmail.com>
2025-11-20 12:55:06 +0530

    dsa

--- src/Greedy/Q51.java
+++ src/Greedy/Q51.java
@@ -2,11 +2,39 @@ package Greedy;
 
 public class Q51 {
 
-    static void maxOperations(String s) {
+    static int maxOperations(String s) {
 
-        //  Time Complexity: O()
-        //  Space Complexity: O()
+        //  Time Complexity: O(n)
+        //  Space Complexity: O(1)
 
+        int n = s.length();
+        int count1seen = 0;
+        int maxOperation = 0;
+        int i = 0;
+
+        while (i < n) {
+
+            if (s.charAt(i) == '1') {
+
+                count1seen++;
+                i++;
+            } else {
+
+                maxOperation += count1seen;
+
+                while (i < n && s.charAt(i) == '0') {
+
+                    i++;
+
+
+                }
+
+
+            }
+
+        }
+
+        return maxOperation;
 
 
     }
@@ -17,7 +45,7 @@ public class Q51 {
         // leeTCode->3228
 
 
-        String s="1001101";
+        String s = "1001101";
         System.out.println(maxOperations(s));
     }
 }
