9c3f6b29eb28c7a5181dc903d6748a4b2f567307
Tushar Patra <tushar04012002@gmail.com>
2025-12-11 23:50:43 +0530

    dsa

--- src/Recursion/CodeWithMik/Q6.java
+++ src/Recursion/CodeWithMik/Q6.java
@@ -1,28 +1,78 @@
-//package Recursion.CodeWithMik;
-//
-//import java.util.List;
-//
-//public class Q6 {
-//
-//
-//    static void solve(List<String> ans, List<List<String>> result ,  ) {
-//
-//
-//
-//
-//
-//
-//    }
-//
-//    static List<List<String>> solveNQueen(int n) {
-//
-//
-//    }
-//
-//    public static void main(String[] args) {
-//
-//        int n = 4;
-//
-//
-//    }
-//}
+package Recursion.CodeWithMik;
+
+
+public class Q6 {
+
+
+    public static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+
+    }
+
+
+     TreeNode prev = null;
+
+     void flatten(TreeNode root) {
+
+
+         //  Time Complexity: O(n)  Where n = number of nodes in the tree.
+         //  Space Complexity: O(h) (recursion stack)
+
+        if (root == null) return;
+
+        flatten(root.right);
+        flatten(root.left);
+        root.right = prev;
+        root.left = null;
+        prev = root;
+
+
+    }
+
+    static void display(TreeNode head){
+
+        TreeNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.right;
+        }
+        System.out.println("null");
+    }
+
+
+    public static void main(String[] args) {
+
+        TreeNode a = new TreeNode(1);
+        TreeNode b = new TreeNode(2);
+        TreeNode c = new TreeNode(5);
+        TreeNode d = new TreeNode(3);
+        TreeNode e = new TreeNode(4);
+        TreeNode f = new TreeNode(6);
+        TreeNode g = new TreeNode(7);
+
+
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        b.right = e;
+        c.right = f;
+        f.left= g;
+
+        Q6 obj=new Q6();
+        display(a);
+        obj.flatten(a);
+        display(a);
+
+
+
+
+    }
+}
--- src/myPractice.java
+++ src/myPractice.java
@@ -1,28 +1,62 @@
-//import java.util.Stack;
-//
-//public class myPractice {
-//
-//
-//
-//    static int towerOfHonoi(int n,int source,int Auxiliary,int destination) {
-//
-//       towerOfHonoi(n-1,source,destination,Auxiliary);
-//
-//       towerOfHonoi(1,source,Auxiliary,destination);
-//
-//       towerOfHonoi(n-1,Auxiliary,source,destination);
-//
-//
-//
-//
-//    }
-//
-//    public static void main(String[] args) {
-//
-//
-//
-//    }
-//
-//
-//}
-//
+import java.util.ArrayList;
+import java.util.List;
+
+public class myPractice {
+
+
+    static char[] direction = {'U', 'L', 'D', 'R'};
+    static int[] dRow = {-1, 0, 1, 0};
+    static int[] dCol = {0, -1, 0, 1};
+
+    static void rateInAMaze(int n, int[][] maze, int rows, int cols, String path, List<String> result) {
+
+        if (rows >= n || cols >= n || rows < 0 || cols < 0 || maze[rows][cols] == 0) return;
+
+        if (rows == n - 1 && cols == n - 1) {
+
+            result.add(path);
+            return;
+        }
+
+
+        maze[rows][cols] = 0;
+
+
+        for (int i = 0; i < 4; i++) {
+
+            int nextRow = rows + dRow[i];
+            int nextCol = cols + dCol[i];
+            rateInAMaze(n, maze, nextRow, nextCol, path + direction[i], result);
+        }
+
+        maze[rows][cols] = 1;
+
+
+    }
+
+    static List<String> rateInAMaze(int[][] maze) {
+
+        List<String> possiblePath = new ArrayList<>();
+
+        if (maze[0][0] == 1) {
+            rateInAMaze(maze.length, maze, 0, 0, "", possiblePath);
+
+        }
+
+        return possiblePath;
+
+    }
+
+    public static void main(String[] args) {
+
+        int[][] maze = {{1, 0, 0, 0}, {1, 1, 0, 1}, {1, 1, 0, 0}, {0, 1, 1, 1}};
+        System.out.println(rateInAMaze(maze));
+
+
+
+
+    }
+
+
+}
+
