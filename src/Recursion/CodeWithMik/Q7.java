62add060be01a176317ed86c336f5b0d5ad52926
Tushar Patra <tushar04012002@gmail.com>
2025-12-12 15:19:47 +0530

    dsa

---
+++ src/Recursion/CodeWithMik/Q7.java
@@ -0,0 +1,107 @@
+package Recursion.CodeWithMik;
+
+import javax.swing.event.ListDataEvent;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Q7 {
+
+    static boolean isValid(String s) {
+
+        Stack<Character> st = new Stack<>();
+
+        int length = s.length();
+
+        for (int i = 0; i < length; i++) {
+
+            char ch = s.charAt(i);
+
+            if (ch == '(') st.push(ch);
+
+            else if (ch == ')') {
+
+                if (st.isEmpty()) return false;
+                st.pop();
+            }
+
+        }
+
+        return st.isEmpty();
+    }
+
+
+    static void generate(String curr, int n, List<String> ans) {
+
+
+        if (curr.length() == 2 * n) {
+            if (isValid(curr)) ans.add(curr);
+            return;
+
+        }
+
+
+        generate(curr + '(', n, ans);
+        generate(curr + ')', n, ans);
+
+
+    }
+
+
+    static List<String> generateParenthesis(int n) {
+
+        //  Time Complexity: O(2^2n * n)
+        //  Space Complexity:  O(2*n)
+
+
+
+        List<String> result = new ArrayList<>();
+        generate("", n, result);
+
+        return result;
+
+
+    }
+
+    static void generateI(String curr, int n, int open, int close, List<String> ans) {
+
+        if (curr.length() == 2 * n) {
+            ans.add(curr);
+            return;
+        }
+
+        if (open < n) generateI(curr + '(', n, open + 1, close, ans);
+
+        if (close < open) generateI(curr + ')', n, open, close + 1, ans);
+
+
+    }
+
+
+    static List<String> generateParenthesisI(int n) {
+
+        //  Time Complexity: O(2^2n * n)
+        //  Space Complexity:  O(2n)
+
+
+        List<String> result = new ArrayList<>();
+        generateI("", n, 0, 0, result);
+
+        return result;
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        int n = 3;
+        List<String> result = generateParenthesis(n);
+        System.out.println(result);
+
+        List<String> result1=generateParenthesisI(n);
+        System.out.println(result1);
+
+
+    }
+}
--- src/myPractice.java
+++ src/myPractice.java
@@ -1,58 +1,111 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class myPractice {
 
+    public static class Node {
 
-    static char[] direction = {'U', 'L', 'D', 'R'};
-    static int[] dRow = {-1, 0, 1, 0};
-    static int[] dCol = {0, -1, 0, 1};
+        int val;
+        Node next;
+        Node child;
 
-    static void rateInAMaze(int n, int[][] maze, int rows, int cols, String path, List<String> result) {
+        Node(int val) {
 
-        if (rows >= n || cols >= n || rows < 0 || cols < 0 || maze[rows][cols] == 0) return;
+            this.val = val;
+        }
+    }
 
-        if (rows == n - 1 && cols == n - 1) {
+    static Node merge(Node head1, Node head2) {
 
-            result.add(path);
-            return;
+        Node temp1 = head1;
+        Node temp2 = head2;
+        Node dummy = new Node(-1);
+        Node p1 = dummy;
+
+        while (temp1 != null && temp2 != null) {
+
+            if (temp1.val < temp2.val) {
+
+                p1.child = temp1;
+                p1 = p1.child;
+                temp1 = temp1.child;
+            } else {
+
+                p1.child = temp2;
+                p1 = p1.child;
+                temp2 = temp2.child;
+
+            }
+
+            p1.next = null;
         }
 
 
-        maze[rows][cols] = 0;
+        if (temp1!=null)  p1.child=temp1;
+        else p1.child=temp2;
+
+        return dummy.child;
 
 
-        for (int i = 0; i < 4; i++) {
-
-            int nextRow = rows + dRow[i];
-            int nextCol = cols + dCol[i];
-            rateInAMaze(n, maze, nextRow, nextCol, path + direction[i], result);
-        }
-
-        maze[rows][cols] = 1;
 
 
     }
 
-    static List<String> rateInAMaze(int[][] maze) {
 
-        List<String> possiblePath = new ArrayList<>();
+    static Node flatten(Node head) {
 
-        if (maze[0][0] == 1) {
-            rateInAMaze(maze.length, maze, 0, 0, "", possiblePath);
+        if (head == null || head.next == null) return head;
 
+        Node newHead = flatten(head.next);
+
+        return merge(head, newHead);
+
+
+    }
+
+    static void display(Node head) {
+
+        Node temp = head;
+        while (temp != null) {
+
+            System.out.print(temp.val + " ");
+            temp = temp.child;
         }
 
-        return possiblePath;
-
+        System.out.println();
     }
 
     public static void main(String[] args) {
 
-        int[][] maze = {{1, 0, 0, 0}, {1, 1, 0, 1}, {1, 1, 0, 0}, {0, 1, 1, 1}};
-        System.out.println(rateInAMaze(maze));
+        Node a = new Node(3);
+        Node b = new Node(2);
+        Node c = new Node(10);
+        Node d = new Node(1);
+        Node e = new Node(7);
+        Node f = new Node(11);
+        Node g = new Node(12);
+        Node h = new Node(4);
+        Node i = new Node(9);
+        Node j = new Node(5);
+        Node k = new Node(6);
+        Node l = new Node(8);
 
 
+        a.next = b;
+        a.child = null;
+        b.next = d;
+        b.child = c;
+        c.child = null;
+        d.child = e;
+        e.child = f;
+        f.child = g;
+        d.next = h;
+        h.child = i;
+        i.child = null;
+        h.next = j;
+        j.child = k;
+        k.child = l;
+        l.child = null;
+
+        Node head = flatten(a);
+        display(head);
 
 
     }
