a8c003075acee085fdfdb119c4c4e79c3663ad46
Tushar Patra <tushar04012002@gmail.com>
2025-12-17 16:36:16 +0530

    dsa

---
+++ src/Recursion/CodeWithMik/Q19.java
@@ -0,0 +1,55 @@
+package Recursion.CodeWithMik;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q19 {
+
+    static void solve(int currNum, int n, List<Integer> result) {
+
+
+        if (currNum > n) return;
+
+        result.add(currNum);
+
+        for (int append = 0; append <= 9; append++) {
+
+            int newNum = (currNum * 10) + append;
+
+            if (newNum > n) return;
+
+            solve(newNum, n, result);
+        }
+
+
+    }
+
+    static List<Integer> lexicoOrder(int n) {
+
+        //  Time Complexity (TC): O(n)
+        //  Space Complexity (SC): O(n)
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int start = 1; start <= 9; start++) {
+
+            solve(start, n, result);
+
+
+        }
+
+        return result;
+
+
+    }
+
+    public static void main(String[] args) {
+
+        int n=13;
+        List<Integer> ans=lexicoOrder(n);
+        System.out.println(ans);
+
+
+    }
+
+}
