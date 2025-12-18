c684addc46ab1fd6942ab0e4aa968ffc5116a5a9
Tushar Patra <tushar04012002@gmail.com>
2025-12-19 02:44:51 +0530

    dsa

--- src/Recursion/CodeWithMik/Q22.java
+++ src/Recursion/CodeWithMik/Q22.java
@@ -2,8 +2,46 @@ package Recursion.CodeWithMik;
 
 public class Q22 {
 
+    static char kthCharacter(long k, int[] operations) {
+
+        //  Time Complexity (TC): O(n.log k)
+        //  Space Complexity (SC): O(log k)
+
+        if (k == 1) return 'a';
+
+        long length = 1;
+        int operationType = 0;
+        long newK = k;
+
+        for (int i = 0; i < operations.length; i++) {
+
+            length *= 2;
+
+            if (length >= k) {
+
+                operationType = operations[i];
+                newK = k - length / 2;
+                break;
+            }
+
+        }
+
+        char ch = kthCharacter(newK, operations);
+
+        if (operationType == 1)  return ch == 'z' ? 'a' : (char) (ch + 1);
+
+        return ch;
+
+
+
+    }
+
     public static void main(String[] args) {
 
         // leeTCode->3307
+
+        long k = 10;
+        int[] operations = {0, 1, 0, 1};
+        System.out.println(kthCharacter(k,operations));
     }
 }
