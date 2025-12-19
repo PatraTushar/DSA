598b3945c7d243ddaeee28db9a2480bb808e4ade
Tushar Patra <tushar04012002@gmail.com>
2025-12-19 13:48:12 +0530

    dsa

---
+++ src/Recursion/CodeWithMik/Q23.java
@@ -0,0 +1,37 @@
+package Recursion.CodeWithMik;
+
+public class Q23 {
+
+    static char findKthBit(int n, int k) {
+
+        //  Time Complexity (TC): O(log k)
+        //  Space Complexity (SC): O(log k)
+
+        if (n == 1) return '0';
+
+        int length = (1 << n) - 1;
+        int half = (length + 1) / 2;
+
+        char ch;
+        if (k == half) return '1';
+        else if (k < half) ch = findKthBit(n - 1, k);
+        else {
+            ch = findKthBit(n - 1, length - k + 1);
+            return ch == '1' ? '0' : '1';
+
+
+        }
+
+        return ch;
+
+
+    }
+
+    public static void main(String[] args) {
+
+        int n = 4;
+        int k = 11;
+        System.out.println(findKthBit(n, k));
+
+    }
+}
