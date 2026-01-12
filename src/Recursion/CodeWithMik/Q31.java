01352e92d6a374ce768b131da25ae94a0ae44690
Tushar Patra <tushar04012002@gmail.com>
2026-01-12 10:24:00 +0530

    dsa

--- src/Recursion/CodeWithMik/Q31.java
+++ src/Recursion/CodeWithMik/Q31.java
@@ -2,7 +2,57 @@ package Recursion.CodeWithMik;
 
 public class Q31 {
 
+    static int minUnfairness(int[] cookies, int k, int index, int[] children, int result) {
+
+
+        if (index >= cookies.length) {
+
+            int unfairness = 0;
+            for (int i = 0; i < k; i++) {
+
+                unfairness = Math.max(unfairness, children[i]);
+            }
+
+            return Math.min(result, unfairness);
+
+        }
+
+        int cookie = cookies[index];
+
+        for (int i = 0; i < k; i++) {
+
+            if (children[i] + cookie >= result) continue;
+
+            children[i] += cookie;
+            result = minUnfairness(cookies, k, index + 1, children, result);
+            children[i] -= cookie;
+
+            if (children[i] == 0) break;
+        }
+
+        return result;
+
+
+    }
+
+
+    static int distributeCookies(int[] cookies, int k) {
+
+        //  Time Complexity (TC): O(k^n)
+        //  Space Complexity (SC): O(n+k)
+
+
+        return minUnfairness(cookies, k, 0, new int[k], Integer.MAX_VALUE);
+
+
+    }
+
+
     public static void main(String[] args) {
+
+        int[] cookies = {8, 15, 10};
+        int k = 2;
+        System.out.println(distributeCookies(cookies, k));
 
 
     }
---
+++ src/Recursion/CodeWithMik/Q32.java
@@ -0,0 +1,9 @@
+package Recursion.CodeWithMik;
+
+public class Q32 {
+
+
+    public static void main(String[] args) {
+
+    }
+}
