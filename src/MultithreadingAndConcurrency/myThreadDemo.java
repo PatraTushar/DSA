cb2c213ae494f3e76efe5f1e7930b532e87ed862
Tushar Patra <tushar04012002@gmail.com>
2026-01-13 01:01:14 +0530

    dsa

---
+++ src/MultithreadingAndConcurrency/myThreadDemo.java
@@ -0,0 +1,32 @@
+package MultithreadingAndConcurrency;
+
+
+class myThread extends Thread {
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println(" Child Thread");
+        }
+    }
+
+
+}
+
+public class myThreadDemo {
+
+    public static void main(String[] args) {
+
+        myThread obj = new myThread();
+        obj.start();
+
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println(" Main Thread ");
+        }
+
+
+    }
+}
--- src/Recursion/CodeWithMik/Q32.java
+++ src/Recursion/CodeWithMik/Q32.java
@@ -1,39 +1,39 @@
-package Recursion.CodeWithMik;
-
-public class Q32 {
-
-    static int minUnfairness(int[] cookies, int k, int index, int[] children, int result) {
-
-        if (index >= cookies.length) {
-
-
-        }
-
-
-        int cookie = cookies[index];
-
-
-        for (int i = 0; i < k; i++) {
-
-            children[i] += cookie;
-            minUnfairness(cookies,k,index+1,children,result);
-
-
-        }
-
-
-    }
-
-
-    static int distributeCookies(int[] cookies, int k) {
-
-        return minUnfairness(cookies, k, 0, new int[k], Integer.MAX_VALUE);
-
-
-    }
-
-
-    public static void main(String[] args) {
-
-    }
-}
+//package Recursion.CodeWithMik;
+//
+//public class Q32 {
+//
+//    static int minUnfairness(int[] cookies, int k, int index, int[] children, int result) {
+//
+//        if (index >= cookies.length) {
+//
+//
+//        }
+//
+//
+//        int cookie = cookies[index];
+//
+//
+//        for (int i = 0; i < k; i++) {
+//
+//            children[i] += cookie;
+//            minUnfairness(cookies,k,index+1,children,result);
+//
+//
+//        }
+//
+//
+//    }
+//
+//
+//    static int distributeCookies(int[] cookies, int k) {
+//
+//        return minUnfairness(cookies, k, 0, new int[k], Integer.MAX_VALUE);
+//
+//
+//    }
+//
+//
+//    public static void main(String[] args) {
+//
+//    }
+//}
