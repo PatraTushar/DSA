22ad22fd4cff6de400a7354e0ab01041b1d60a10
Tushar Patra <tushar04012002@gmail.com>
2026-01-13 10:44:47 +0530

    dsa

---
+++ src/MultithreadingAndConcurrency/myThreadDemoA.java
@@ -0,0 +1,34 @@
+package MultithreadingAndConcurrency;
+
+
+// overloading the run() method
+class myThreadA extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println(" Child Method ");
+    }
+
+    public void run(int a) {
+
+        System.out.println(" int-Argument method ");
+
+    }
+
+}
+
+public class myThreadDemoA {
+
+    public static void main(String[] args) {
+
+        myThreadA obj = new myThreadA();
+        obj.start();
+
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println(" main method ");
+        }
+    }
+
+
+}
