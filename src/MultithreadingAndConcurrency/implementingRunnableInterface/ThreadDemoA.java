95ce63c49e80c9513260dfa926cc9dcb992a6d2c
Tushar Patra <tushar04012002@gmail.com>
2026-01-13 14:43:04 +0530

    dsa

---
+++ src/MultithreadingAndConcurrency/implementingRunnableInterface/ThreadDemoA.java
@@ -0,0 +1,34 @@
+package MultithreadingAndConcurrency.implementingRunnableInterface;
+
+
+class myRunnableA implements Runnable {
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println(" Child Method");
+        }
+    }
+}
+public class ThreadDemoA {
+
+    public static void main(String[] args) {
+
+        myRunnableA obj=new myRunnableA();
+        Thread t1=new Thread();
+        t1.start();
+        t1.run();
+
+        Thread t2=new Thread(obj);
+        t2.start();
+        t2.run();
+
+        obj.run();
+       // obj.start();
+
+
+
+    }
+}
