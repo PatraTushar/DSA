300d1e09663f35ee443332889d09f69f6fc5ca38
Tushar Patra <tushar04012002@gmail.com>
2026-01-14 14:23:28 +0530

    dsa

--- src/Recursion/CodeWithMik/Q32.java
+++ src/Recursion/CodeWithMik/Q32.java
@@ -1,39 +1,60 @@
-//package Recursion.CodeWithMik;
-//
-//public class Q32 {
-//
-//    static int minUnfairness(int[] cookies, int k, int index, int[] children, int result) {
-//
-//        if (index >= cookies.length) {
-//
-//
-//        }
-//
-//
-//        int cookie = cookies[index];
-//
-//
-//        for (int i = 0; i < k; i++) {
-//
-//            children[i] += cookie;
-//            minUnfairness(cookies,k,index+1,children,result);
-//
-//
-//        }
-//
-//
-//    }
-//
-//
-//    static int distributeCookies(int[] cookies, int k) {
-//
-//        return minUnfairness(cookies, k, 0, new int[k], Integer.MAX_VALUE);
-//
-//
-//    }
-//
-//
-//    public static void main(String[] args) {
-//
-//    }
-//}
+package Recursion.CodeWithMik;
+
+public class Q32 {
+
+    int ans = 0;
+
+    void solve(int[][] requests, int index, int[] balance, int count) {
+
+        if (index == requests.length) {
+
+            for (int ele : balance) {
+
+                if (ele != 0) return;
+            }
+
+            ans = Math.max(ans, count);
+            return;
+        }
+
+
+        int from = requests[index][0];
+        int to = requests[index][1];
+
+        balance[from]--;
+        balance[to]++;
+
+        solve(requests, index + 1, balance, count + 1);
+
+        balance[from]++;
+        balance[to]--;
+
+        solve(requests, index + 1, balance, count);
+
+
+    }
+
+    int maximumRequests(int n, int[][] requests) {
+
+        //  Time Complexity (TC): O(2^R * n)
+        //  Space Complexity (SC): O(R + N)
+
+
+        int[] balance = new int[n];
+        solve(requests, 0, balance, 0);
+        return ans;
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        Q32 obj=new Q32();
+
+        int[][] requests={{0,1},{1,0},{0,1},{1,2},{2,0},{3,4}};
+        int n=5;
+        System.out.println(obj.maximumRequests(n,requests));
+
+    }
+}
