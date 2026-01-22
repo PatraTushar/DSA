f83fa78ab9a6e859d5041ef4d9cccd9c39ca7018
Tushar Patra <tushar04012002@gmail.com>
2026-01-23 00:59:37 +0530

    dsa

--- src/MultithreadingAndConcurrency/multiThreadingEnhancement/ThreadLocalDemoD.java
+++ src/MultithreadingAndConcurrency/multiThreadingEnhancement/ThreadLocalDemoD.java
@@ -1,8 +1,44 @@
 package MultithreadingAndConcurrency.multiThreadingEnhancement;
 
+class ParentThreadA extends Thread {
+
+  public static InheritableThreadLocal tl=new InheritableThreadLocal(){
+
+        public Object childValue(Object p){
+
+            return "CC";
+
+        }
+    };
+
+    @Override
+    public void run() {
+
+        tl.set("PP");
+        System.out.println(" parent value : "+tl.get());
+
+
+        ChildThreadA ct=new ChildThreadA();
+        ct.start();
+    }
+}
+
+class ChildThreadA extends Thread {
+
+    @Override
+    public void run() {
+
+        System.out.println(" child value : " +ParentThreadA.tl.get());
+    }
+}
+
 public class ThreadLocalDemoD {
 
     public static void main(String[] args) {
+
+        ParentThreadA pt=new ParentThreadA();
+        pt.start();
+
 
     }
 }
