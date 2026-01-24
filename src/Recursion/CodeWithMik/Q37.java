c34081bbfc0b63005aa4d044139143c2076d4b6f
Tushar Patra <tushar04012002@gmail.com>
2026-01-24 16:19:22 +0530

    dsa

--- src/Recursion/CodeWithMik/Q37.java
+++ src/Recursion/CodeWithMik/Q37.java
@@ -4,47 +4,39 @@ import java.util.HashMap;
 
 public class Q37 {
 
-    static int dfs(int[] nums, int k, int index, HashMap<Integer, Integer> freq) {
-
-        if (index == nums.length) {
-
-            if (freq.size() == 0) return 0;
-            return 1;
-        }
-
-
-        int pick = 0;
-        int element1 = nums[index] + k;
-        int element2 = nums[index] - k;
-
-        if (freq.size() == 0 || (!freq.containsKey(element1)) && !freq.containsKey(element2)) {
-            // include
-            freq.put(nums[index], freq.getOrDefault(nums[index], 0) + 1);
-
-            // explore
-            pick = dfs(nums, k, index + 1, freq);
-
-            // backtrack
-            freq.put(nums[index], freq.get(nums[index])-1);
-
-            if (freq.get(nums[index]) == 0) {
-
-                freq.remove(nums[index]);
-            }
-        }
-
-        int notPick = dfs(nums, k, index + 1, freq);
-        return pick + notPick;
-
-
-    }
-
 
     static int beautifulSubsets(int[] nums, int k) {
+        return dfs(nums, k, 0, new HashMap<>());
+    }
 
-        return dfs(nums,k,0,new HashMap<>());
+  static int dfs(int[] nums, int k, int idx, HashMap<Integer, Integer> freq) {
 
+        // Base case: all elements processed
+        if (idx == nums.length) {
+            // empty subset is NOT allowed
+            return freq.isEmpty() ? 0 : 1;
+        }
 
+        int count = 0;
+
+        // ---------------- NOT TAKE ----------------
+        count += dfs(nums, k, idx + 1, freq);
+
+        // ---------------- TAKE ----------------
+        int val = nums[idx];
+
+        if (!freq.containsKey(val - k) && !freq.containsKey(val + k)) {
+
+            freq.put(val, freq.getOrDefault(val, 0) + 1);
+
+            count += dfs(nums, k, idx + 1, freq);
+
+            // backtrack
+            freq.put(val, freq.get(val) - 1);
+            if (freq.get(val) == 0) freq.remove(val);
+        }
+
+        return count;
     }
 
 
@@ -52,7 +44,7 @@ public class Q37 {
 
         int[] nums = {2, 4, 6};
         int k = 2;
-        System.out.println(beautifulSubsets(nums,k));
+        System.out.println(beautifulSubsets(nums, k));
 
     }
 }
