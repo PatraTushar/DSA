7962f8fc42d5c62fa90b81de86e37ae891c0b0d2
Tushar Patra <tushar04012002@gmail.com>
2026-01-31 11:55:07 +0530

    dsa

--- src/Recursion/CodeWithMik/Q40.java
+++ src/Recursion/CodeWithMik/Q40.java
@@ -43,6 +43,8 @@ public class Q40 {
 
     public static void main(String[] args) {
 
+        // leeTCode->40
+
         int[] candidates = {10, 1, 2, 7, 6, 1, 5};
         int target = 8;
         System.out.println(combinationSum2(candidates,target));
---
+++ src/Recursion/CodeWithMik/Q41.java
@@ -0,0 +1,11 @@
+package Recursion.CodeWithMik;
+
+public class Q41 {
+
+    public static void main(String[] args) {
+
+        // leeTCode->1593
+
+
+    }
+}
