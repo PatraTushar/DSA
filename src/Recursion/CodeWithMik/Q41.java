59fe5d657446704b8b75c85a6c2a59b8d9730d99
Tushar Patra <tushar04012002@gmail.com>
2026-01-31 21:50:12 +0530

    dsa

--- src/Recursion/CodeWithMik/Q41.java
+++ src/Recursion/CodeWithMik/Q41.java
@@ -1,10 +1,50 @@
 package Recursion.CodeWithMik;
 
+
+import java.util.HashSet;
+import java.util.Map;
+
 public class Q41 {
+
+    static int solve(String s, int start, HashSet<String> set) {
+
+        if (start == s.length()) return set.size();
+
+        int max=0;
+
+        for (int end = start + 1; end <= s.length(); end++) {
+
+            String subString=s.substring(start,end);
+
+            if (!set.contains(subString)){
+
+                set.add(subString);
+                int nextPath=solve(s,end,set);
+                max= Math.max(max,nextPath);
+                set.remove(subString);
+            }
+
+
+        }
+
+        return max;
+    }
+
+    static int maxUniqueSplit(String s) {
+
+        //  Time Complexity (TC): O( 2^n)
+        //  Space Complexity (SC): O(n^2)
+
+        return solve(s,0,new HashSet<>());
+
+    }
 
     public static void main(String[] args) {
 
         // leeTCode->1593
+
+        String s="ababccc";
+        System.out.println(maxUniqueSplit(s));
 
 
     }
