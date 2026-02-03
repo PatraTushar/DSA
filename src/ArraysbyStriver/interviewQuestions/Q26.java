e519f2beb424bd1b35a0316347fdcc55a6fd3901
Tushar Patra <tushar04012002@gmail.com>
2026-02-03 23:40:54 +0530

    dsa

--- src/ArraysbyStriver/interviewQuestions/Q26.java
+++ src/ArraysbyStriver/interviewQuestions/Q26.java
@@ -4,13 +4,13 @@ public class Q26 {
 
 
     // BruteForce Method  (Method->1)
-    static int sumOfRectangles(int[][] arr,int r1,int c1,int r2,int c2){
+    static int sumOfRectangles(int[][] arr, int r1, int c1, int r2, int c2) {
 
-        int sum=0;
+        int sum = 0;
 
-        for(int i=r1;i<=r2;i++){
-            for(int j=c1;j<=c2;j++){
-                sum+=arr[i][j];
+        for (int i = r1; i <= r2; i++) {
+            for (int j = c1; j <= c2; j++) {
+                sum += arr[i][j];
             }
         }
 
@@ -19,37 +19,36 @@ public class Q26 {
     }
 
 
-
     // Method-2 (Optimal approach)
 
-    static int calculateSum(int[][] arr,int r1,int r2,int c1,int c2){
+    static int calculateSum(int[][] arr, int r1, int r2, int c1, int c2) {
 
         if (r1 < 0 || r2 < 0 || c1 < 0 || c2 < 0) {
             throw new IllegalArgumentException("Negative indices are not allowed");
         }
 
-        int row=arr.length;
-        int column=arr[0].length;
+        int row = arr.length;
+        int column = arr[0].length;
 
         // prefixSum in rowWise
 
-        for(int i=0;i<arr.length;i++){
-            for(int j=1;j<arr[0].length;j++){
-                arr[i][j]+=arr[i][j-1];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr[0].length; j++) {
+                arr[i][j] += arr[i][j - 1];
             }
         }
 
         // prefixSum In ColumnWise
 
 
-            for(int j=0;j<column;j++) {
-                for (int i = 1; i < row; i++) {
-                    arr[i][j] += arr[i - 1][j];
-                }
+        for (int j = 0; j < column; j++) {
+            for (int i = 1; i < row; i++) {
+                arr[i][j] += arr[i - 1][j];
             }
+        }
 
 
-       int  sum = arr[r2][c2];
+        int sum = arr[r2][c2];
         if (r1 > 0) sum -= arr[r1 - 1][c2];
         if (c1 > 0) sum -= arr[r2][c1 - 1];
         if (r1 > 0 && c1 > 0) sum += arr[r1 - 1][c1 - 1];
@@ -57,29 +56,18 @@ public class Q26 {
         return sum;
 
 
-
-
-
-
-
     }
-
-
-
-
-
 
 
     public static void main(String[] args) {
 
         // Q: sum of rectangles between the coordinates (pattern: prefix sum in 2D arrays)
-        int[][] arr={{1,1,1,1,1,1,1},{1,1,1,1,1,1,1},{1,1,1,1,1,1,1},{1,1,1,1,1,1,1},{1,1,1,1,1,1,1},{1,1,1,1,1,1,1},{1,1,1,1,1,1,1}};
-        int r1=3;
-        int r2=5;
-        int c1=1;
-        int c2=4;
-        System.out.println(calculateSum(arr,r1,r2,c1,c2));
-
+        int[][] arr = {{1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1}};
+        int r1 = 3;
+        int r2 = 5;
+        int c1 = 1;
+        int c2 = 4;
+        System.out.println(calculateSum(arr, r1, r2, c1, c2));
 
 
     }
--- src/myPractice.java
+++ src/myPractice.java
@@ -3,8 +3,42 @@ import java.util.Arrays;
 public class myPractice {
 
 
-    static void func(int[][] num) {
+    static int func(int[][] num, int r1, int c1, int r2, int c2) {
 
+        int row = num.length;
+        int col = num[0].length;
+
+        for (int rowWise = 0; rowWise < row; rowWise++) {
+
+            for (int columnWise = 1; columnWise < col; columnWise++) {
+
+                num[rowWise][columnWise] += num[rowWise][columnWise - 1];
+
+
+            }
+        }
+
+        for (int columnWise = 0; columnWise < col; columnWise++) {
+
+            for (int rowWise = 1; rowWise < row; rowWise++) {
+
+                num[rowWise][columnWise] += num[rowWise - 1][columnWise];
+
+
+            }
+
+
+        }
+
+
+        int sum = num[r2][c2];
+
+        if (r1 > 0) sum -= num[r1 - 1][c2];
+        if (c1 > 0) sum -= num[r2][c1 - 1];
+        if (r1>0 && c1>0) sum+=num[r1-1][c1-1];
+
+
+        return sum;
 
 
     }
@@ -12,8 +46,9 @@ public class myPractice {
 
     public static void main(String[] args) {
 
-
-
+        int[][] arr = {{1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1}};
+        int res = func(arr,3,1,5,4);
+        System.out.println(res);
 
 
     }
