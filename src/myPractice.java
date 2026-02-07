ee37e8bcdf745e571ba792499f8a6fea90f694fd
Tushar Patra <tushar04012002@gmail.com>
2026-02-07 14:27:31 +0530

    dsa

--- src/myPractice.java
+++ src/myPractice.java
@@ -1,40 +1,26 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 
 public class myPractice {
 
 
     static int func(int[] nums) {
 
-        int maxConsecutiveOnes = 0;
-        int count = 0;
+        int unique=0;
 
-        for (int ele : nums) {
+        for (int ele : nums ){
 
-            if (ele == 1) count++;
-
-            else {
-
-                maxConsecutiveOnes = Math.max(maxConsecutiveOnes, count);
-
-                count = 0;
-            }
-
-
+            unique^=ele;
         }
 
-        maxConsecutiveOnes = Math.max(maxConsecutiveOnes, count);
+        return unique;
 
-        return maxConsecutiveOnes;
+
 
     }
 
 
     public static void main(String[] args) {
 
-        int[] arr = {1,1,0,0,0,1,1,1,1,1,1,0};
+        int[] arr = {1,1,2,3,3,4,4};
         System.out.println(func(arr));
 
 
