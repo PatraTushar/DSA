1903dc27f628496e81fb80f95b48c1c8ec7d7c4e
Tushar Patra <tushar04012002@gmail.com>
2026-02-16 11:07:28 +0530

    dsa

---
+++ src/Java_Collection_FrameWork/ComparableAndComparator.java
@@ -0,0 +1,84 @@
+package Java_Collection_FrameWork;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
+class Employee implements Comparable<Object> {
+
+    String name;
+    int empID;
+
+    Employee(String name, int empID) {
+
+        this.name = name;
+        this.empID = empID;
+    }
+
+    public String toString() {
+
+        return name + "--" + empID;
+    }
+
+
+    @Override
+    public int compareTo(Object obj1) {
+
+        int empID1 = this.empID;
+        Employee e = (Employee) obj1;
+        int empID2 = e.empID;
+
+        if (empID1 < empID2) return -1;
+        else if (empID1 > empID2) return +1;
+        else return 0;
+
+    }
+
+
+}
+
+
+class MyComparator implements Comparator<Object> {
+
+    @Override
+    public int compare(Object obj1, Object obj2) {
+
+        Employee e1 = (Employee) obj1;
+        Employee e2 = (Employee) obj2;
+        String s1 = e1.name;
+        String s2 = e2.name;
+        return s1.compareTo(s2);
+
+    }
+}
+
+public class ComparableAndComparator {
+
+    public static void main(String[] args) {
+
+
+        TreeSet<Employee> t = new TreeSet<>();
+        Employee e1 = new Employee("nag", 100);
+        Employee e2 = new Employee("balaiah", 200);
+        Employee e3 = new Employee("chinu", 50);
+        Employee e4 = new Employee("venki", 100);
+        Employee e5 = new Employee("nag", 100);
+
+        t.add(e1);
+        t.add(e2);
+        t.add(e3);
+        t.add(e4);
+        t.add(e5);
+        System.out.println(t);
+
+
+        TreeSet<Employee> t1 = new TreeSet<>(new MyComparator());
+        t1.add(e1);
+        t1.add(e2);
+        t1.add(e3);
+        t1.add(e4);
+        t1.add(e5);
+        System.out.println(t1);
+
+
+    }
+}
