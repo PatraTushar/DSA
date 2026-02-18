a705c18fed73414aaf38804adf6b8aa295523560
Tushar Patra <tushar04012002@gmail.com>
2026-02-18 12:28:46 +0530

    dsa

---
+++ src/Java_Collection_FrameWork/ConcurrentCollection.java
@@ -0,0 +1,44 @@
+package Java_Collection_FrameWork;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ConcurrentCollection {
+}
+
+class myThread extends Thread {
+
+    static ArrayList<String> l=new ArrayList<>();
+
+    @Override
+    public void run() {
+      try {
+          Thread.sleep(2000);
+      }catch (InterruptedException e){}
+
+        System.out.println(" child thread updating list ");
+      l.add("D");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        l.add("A");
+        l.add("B");
+        l.add("C");
+
+        myThread t=new myThread();
+        t.start();
+
+        Iterator<String> itr=l.iterator();
+
+        while (itr.hasNext()){
+
+            String s= itr.next();
+
+            System.out.println(" main thread iterating list and current object is :"+s);
+            Thread.sleep(3000);
+        }
+
+        System.out.println(l);
+    }
+}
