c557d9dc7f859b941e70ee8503d196eeb408cf91
Tushar Patra <tushar04012002@gmail.com>
2026-03-21 19:19:36 +0530

    dsa

--- src/ARRAYS/binarySearchPattern/Q8.java
+++ src/ARRAYS/binarySearchPattern/Q8.java
@@ -12,21 +12,19 @@ public class Q8 {
         int low = 0;
         int high = n - 1;
 
-        if (n == 1) return low;
-
         while (low <= high) {
 
             int mid = low + (high - low) / 2;
 
             if ((mid > 0 && arr[mid] > arr[mid - 1]) && (mid < n - 1 && arr[mid] > arr[mid + 1])) return mid;
 
-            else if ((mid < n - 1) && (arr[mid] > arr[mid + 1])) high = mid - 1;
+            else if (mid > 0 && arr[mid] < arr[mid - 1]) high = mid-1;
 
             else low = mid + 1;
+
         }
 
-        return -1;
-
+        return high;
     }
 
     public static void main(String[] args) {
--- src/myPractice.java
+++ src/myPractice.java
@@ -2,57 +2,35 @@ import java.util.Arrays;
 
 public class myPractice {
 
-    static int binarySearch(int[] arr, int startPoint, int endingPoint, int target) {
 
-        int start = startPoint;
-        int end = endingPoint;
+    static int func(int[] arr) {
 
-
-        while (start <= end) {
-
-            int mid = start + (end - start) / 2;
-
-            if (target == arr[mid]) return mid;
-
-            else if (target > arr[mid]) start = mid + 1;
-
-            else end = mid - 1;
-        }
-
-        return -1;
-
-
-    }
-
-
-    static int func(int[] nums, int target) {
-
-        int n = nums.length;
+        int n = arr.length;
 
         int low = 0;
-        int high = 1;
+        int high = n - 1;
 
-        while (high < n && target > nums[high]) {
+        while (low <= high) {
 
-            low = high;
-            high = 2 * high;
+            int mid = low + (high - low) / 2;
 
+            if ((mid > 0 && arr[mid] > arr[mid - 1]) && (mid < n - 1 && arr[mid] > arr[mid + 1])) return mid;
 
-            if (high >= n) high = n - 1;
+            else if (mid > 0 && arr[mid] < arr[mid - 1]) high = mid-1;
 
+            else low = mid + 1;
 
         }
 
-        return binarySearch(nums, low, high, target);
+        return high;
 
 
     }
 
     public static void main(String[] args) {
 
-        int[] arr = {2, 3, 5, 6, 7, 8, 10, 11, 12, 15, 20, 23, 30};
-        int target = 15;
-        System.out.println(func(arr, target));
+        int[] arr = {3, 5, 3, 2, 0};
+        System.out.println(func(arr));
 
 
     }
