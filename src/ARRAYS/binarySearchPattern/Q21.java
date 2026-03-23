141d214a1aad20101f3f14941e0d926a22a29327
Tushar Patra <tushar04012002@gmail.com>
2026-03-24 00:07:57 +0530

    dsa

--- src/ARRAYS/binarySearchPattern/Q21.java
+++ src/ARRAYS/binarySearchPattern/Q21.java
@@ -7,20 +7,17 @@ public class Q21 {
         // Time Complexity: O(rows + col)
         // Space Complexity: O(1)
 
-        int rows=0;
-        int columns=matrix[rows].length-1;
+        int n = matrix.length;
+        int rows = 0;
+        int columns = matrix[rows].length - 1;
 
-        while (rows<matrix.length && columns>=0){
+        while (rows < n && columns >= 0) {
 
-            if(matrix[rows][columns]==target){
+            if (matrix[rows][columns] == target) {
                 return true;
-            }
-
-            else if(target<matrix[rows][columns]){
+            } else if (matrix[rows][columns] > target) {
                 columns--;
-            }
-
-            else {
+            } else {
                 rows++;
             }
         }
@@ -33,8 +30,8 @@ public class Q21 {
 
         // search a 2D matrix II (leeTCode-->240)
 
-        int[][] arr={{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
-        int target=5;
-        System.out.println(searchMatrix(arr,target));
+        int[][] arr = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}};
+        int target = 5;
+        System.out.println(searchMatrix(arr, target));
     }
 }
