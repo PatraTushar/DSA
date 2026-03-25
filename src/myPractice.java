5a9dc6daa3e331f45ac85708f08bcbe413e9431a
Tushar Patra <tushar04012002@gmail.com>
2026-03-26 01:33:58 +0530

    dsa

--- src/myPractice.java
+++ src/myPractice.java
@@ -24,58 +24,92 @@ public class myPractice {
         System.out.println();
     }
 
-    static ListNode nthNodeFromEnd(ListNode head, int n) {
+    static ListNode findIntersection(ListNode headA, ListNode headB) {
 
+        ListNode tempA = headA;
+        ListNode tempB = headB;
 
-        ListNode slow = head;
-        ListNode fast = head;
+        int lengthA = 0;
+        int lengthB = 0;
 
+        while (tempA != null) {
 
-        for (int i = 0; i < n; i++) {
+            lengthA++;
 
-            if (fast == null) return head;
-            fast = fast.next;
-
-
+            tempA = tempA.next;
         }
 
-        if (fast == null) return head.next;
+        while (tempB != null) {
 
-        while (fast.next != null) {
+            lengthB++;
+            tempB = tempB.next;
+        }
 
+        if (lengthA > lengthB) return findIntersection(headB, headA);
+
+        int lengthDiff = lengthB - lengthA;
+
+
+        ListNode slow = headA;
+        ListNode fast = headB;
+
+        for (int i = 0; i < lengthDiff; i++) {
+
+            fast = fast.next;
+        }
+
+
+        while (slow != fast) {
             slow = slow.next;
             fast = fast.next;
         }
 
-        slow.next = slow.next.next;
+        return slow;
 
-        return head;
+    }
 
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
 
+        // one pass solution
+
+        ListNode a = headA;
+        ListNode b = headB;
+
+        while (a != b) {
+            a = (a == null) ? headB : a.next;
+            b = (b == null) ? headA : b.next;
+        }
+
+        return a;
     }
 
 
     public static void main(String[] args) {
 
+        ListNode a = new ListNode(87);
+        ListNode b = new ListNode(100);
+        ListNode c = new ListNode(13);
+        ListNode d = new ListNode(4);
+        ListNode e = new ListNode(5);
+        ListNode f = new ListNode(12);
+        ListNode g = new ListNode(10);
 
-        ListNode a = new ListNode(100);
-        ListNode b = new ListNode(13);
-        ListNode c = new ListNode(4);
-        ListNode d = new ListNode(5);
-        ListNode e = new ListNode(12);
-        ListNode f = new ListNode(10);
         a.next = b;
         b.next = c;
         c.next = d;
         d.next = e;
         e.next = f;
+        f.next = g;
+
+        ListNode h = new ListNode(90);
+        ListNode i = new ListNode(9);
+        h.next = i;
+        i.next = e;
+
 
         display(a);
-
-        nthNodeFromEnd(a, 4);
-
-        display(a);
-
+        ListNode Ans = findIntersection(a, h);
+        System.out.println(" Intersection of both linked list is " + Ans.val);
 
     }
 }
