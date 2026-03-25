c9566e43d0fbb7c7d173e59d9948f739fe069be7
Tushar Patra <tushar04012002@gmail.com>
2026-03-26 00:10:04 +0530

    dsa

--- src/LINKEDLIST/SinglyLinkedList/InterviewQuestion/Q3.java
+++ src/LINKEDLIST/SinglyLinkedList/InterviewQuestion/Q3.java
@@ -2,30 +2,32 @@ package LINKEDLIST.SinglyLinkedList.InterviewQuestion;
 
 public class Q3 {
 
-    public static class Node{
+    // same as Q2
+
+    public static class Node {
 
         int data;
         Node next;
 
-        Node(int data){
+        Node(int data) {
 
-            this.data=data;
+            this.data = data;
         }
 
     }
 
-    static void display(Node head){
+    static void display(Node head) {
 
-        Node temp=head;
-        while(temp!=null){
-            System.out.print(temp.data +" ");
-            temp=temp.next;
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
         }
 
         System.out.println();
     }
 
-    static Node nthNodeFromLast(Node head,int n){
+    static Node nthNodeFromLast(Node head, int n) {
 
         //Time Complexity (TC): O(n)
         //Space Complexity (SC): O(1)
@@ -34,42 +36,41 @@ public class Q3 {
             throw new IllegalArgumentException("Invalid input: List is empty or n is non-positive.");
         }
 
-        Node slow=head;
-        Node fast=head;
+        Node slow = head;
+        Node fast = head;
 
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
 
-            fast=fast.next;
+            fast = fast.next;
         }
 
-        while (fast!=null){
-            slow=slow.next;
-            fast=fast.next;
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
         }
 
         return slow;
     }
 
 
-
     public static void main(String[] args) {
 
         // Q: finding nth node from the end of linked list(in one traversal)
 
-        Node a=new Node(100);
-        Node b=new Node(13);
-        Node c=new Node(4);
-        Node d=new Node(5);
-        Node e=new Node(12);
-        Node f=new Node(10);
-        a.next=b;
-        b.next=c;
-        c.next=d;
-        d.next=e;
-        e.next=f;
+        Node a = new Node(100);
+        Node b = new Node(13);
+        Node c = new Node(4);
+        Node d = new Node(5);
+        Node e = new Node(12);
+        Node f = new Node(10);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
         display(a);
 
-        Node ans=nthNodeFromLast(a,3);
+        Node ans = nthNodeFromLast(a, 3);
         System.out.println(ans.data);
 
 
