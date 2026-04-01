0ec2058f26bf04377f6768b5833cb4c8706de98f
Tushar Patra <tushar04012002@gmail.com>
2026-04-01 16:47:47 +0530

    dsa

--- src/LINKEDLIST/SinglyLinkedList/InterviewQuestion/Q28.java
+++ src/LINKEDLIST/SinglyLinkedList/InterviewQuestion/Q28.java
@@ -8,37 +8,37 @@ public class Q28 {
         int val;
         ListNode next;
 
-        ListNode(int val){
+        ListNode(int val) {
 
             this.val = val;
         }
     }
 
-    static void display(ListNode head){
+    static void display(ListNode head) {
 
-        ListNode temp=head;
-        while (temp!=null){
-            System.out.print(temp.val +" ");
-            temp=temp.next;
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
         }
         System.out.println();
 
     }
 
-    static ListNode reverse(ListNode head){
+    static ListNode reverse(ListNode head) {
 
-        if(head==null || head.next==null) return head;
+        if (head == null || head.next == null) return head;
 
-        ListNode curr=head;
-        ListNode prev=null;
-        ListNode agla=null;
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode agla;
 
-        while (curr!=null){
+        while (curr != null) {
 
-            agla=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=agla;
+            agla = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = agla;
 
         }
 
@@ -46,50 +46,60 @@ public class Q28 {
 
     }
 
-    static void reorderSet(ListNode head){
+    static ListNode findLeftMiddle(ListNode head) {
+
+        ListNode prev = null;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        if (fast == null) return prev;
+
+        return slow;
+
+
+    }
+
+
+    static void reorderSet(ListNode head) {
 
         //Time Complexity (TC): O(n)
         //Space Complexity (SC): O(1)
 
 
+        ListNode middle = findLeftMiddle(head);
 
-        ListNode slow=head;
-        ListNode fast=head;
+        ListNode second = reverse(middle.next);
 
-
-        while ( fast.next!=null && fast.next.next!=null){
-
-            slow=slow.next;
-            fast=fast.next.next;
-        }
+        middle.next = null;
 
 
-        ListNode rev2ndHalf=reverse(slow.next);
-        slow.next=null;
-
-        ListNode pointer1=head;
-        ListNode pointer2=rev2ndHalf;
+        ListNode first = head;
 
 
-
-        while (pointer1 != null &&pointer2!=null){
-
-            ListNode temp1 = pointer1.next;
-            ListNode temp2 = pointer2.next;
-
-            pointer1.next = pointer2;
-            pointer2.next = temp1;
-
-            pointer1 = temp1;
-            pointer2 = temp2;
+        ListNode temp1, temp2;
 
 
+        while (second != null) {
 
+            temp1 = first.next;
+            temp2 = second.next;
+
+
+            first.next = second;
+            second.next = temp1;
+
+
+            first = temp1;
+            second = temp2;
 
         }
-
-        display(head);
-
 
 
     }
@@ -98,24 +108,23 @@ public class Q28 {
 
         // Q: Reorder list (leeTCode->143)
 
-        ListNode a=new ListNode(1);
-        ListNode b=new ListNode(2);
-        ListNode c=new ListNode(3);
-        ListNode d=new ListNode(4);
-        ListNode e=new ListNode(5);
-        ListNode f=new ListNode(6);
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(3);
+        ListNode d = new ListNode(4);
+        ListNode e = new ListNode(5);
+        ListNode f = new ListNode(6);
 
-        a.next=b;
-        b.next=c;
-        c.next=d;
-        d.next=e;
-        e.next=f;
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
 
         display(a);
 
 
         reorderSet(a);
-
 
 
     }
