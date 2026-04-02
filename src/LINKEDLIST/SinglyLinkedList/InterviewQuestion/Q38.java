854e18396b8a6bc10bd2cf18abd39831628dcdfd
Tushar Patra <tushar04012002@gmail.com>
2026-04-02 21:43:24 +0530

    dsa

--- src/LINKEDLIST/SinglyLinkedList/InterviewQuestion/Q38.java
+++ src/LINKEDLIST/SinglyLinkedList/InterviewQuestion/Q38.java
@@ -5,76 +5,64 @@ import java.util.Arrays;
 
 public class Q38 {
 
-    public static class Node {
+    public static class ListNode {
 
 
-        int data;
-        Node prev;
-        Node next;
+        int val;
+        ListNode prev;
+        ListNode next;
 
-        Node(int data) {
+        ListNode(int val) {
 
-            this.data = data;
+            this.val = val;
         }
     }
 
 
-    static ArrayList<ArrayList<Integer>> sumPairs(Node head, int sum){
+    static ArrayList<ArrayList<Integer>> sumPairs(ListNode head, int sum) {
 
         //Time Complexity (TC): O(N)
         //Space Complexity (SC): O(1) (excluding the output list)
 
-        ArrayList<ArrayList<Integer>> list=new ArrayList<>();
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
 
-        Node temp=head;
+        ListNode left = head;
+        ListNode right = head;
 
-        while (temp.next!=null){
+        while (right.next != null) {
 
-            temp=temp.next;
-
+            right = right.next;
         }
 
 
-        while (head != null && temp != null && head != temp && temp.next != head){
+        while (left != null && right != null && left != right && left.prev != right) {
 
-            int pairSum=head.data + temp.data;
+            int currentSum = left.val + right.val;
 
-            if(pairSum==sum){
+            if (currentSum == sum) {
 
-             list.add(new ArrayList<>(Arrays.asList(head.data,temp.data)));
-             head=head.next;
-             temp=temp.prev;
-
-
+                result.add(new ArrayList<>(Arrays.asList(left.val, right.val)));
+                left = left.next;
+                right = right.prev;
             }
 
+            else if (currentSum < sum) left = left.next;
 
-            else if(head.data + temp.data>sum){
-                temp=temp.prev;
-            }
-
-            else {
-                head=head.next;
-            }
-
-
+            else right = right.prev;
         }
 
-        return list;
+        return result;
 
 
     }
 
 
+    static void display(ListNode head) {
 
-
-
-    static void display(Node head){
-
-        Node temp=head;
-        while (temp!=null){
-            System.out.print(temp.data +" ");
-            temp=temp.next;
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
         }
         System.out.println();
     }
@@ -84,31 +72,27 @@ public class Q38 {
 
         // Find pairs with given sum in Sorted DLL
 
-        Node a=new Node(1);
-        Node b=new Node(2);
-        Node c=new Node(3);
-        Node d=new Node(4);
-        Node e=new Node(9);
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(3);
+        ListNode d = new ListNode(4);
+        ListNode e = new ListNode(9);
 
-        a.prev=null;
-        a.next=b;
-        b.prev=a;
-        b.next=c;
-        c.prev=b;
-        c.next=d;
-        d.prev=c;
-        d.next=e;
-        e.prev=d;
-        e.next=null;
+        a.prev = null;
+        a.next = b;
+        b.prev = a;
+        b.next = c;
+        c.prev = b;
+        c.next = d;
+        d.prev = c;
+        d.next = e;
+        e.prev = d;
+        e.next = null;
 
-        int sum=5;
+        int sum = 5;
 
-        ArrayList<ArrayList<Integer>> Ans=sumPairs(a,sum);
+        ArrayList<ArrayList<Integer>> Ans = sumPairs(a, sum);
         System.out.println(Ans);
-
-
-
-
 
 
     }
