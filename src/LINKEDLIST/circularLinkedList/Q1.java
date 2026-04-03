015a6bde8b7a6bdc531495986947b9c593a1780f
Tushar Patra <tushar04012002@gmail.com>
2026-04-03 14:00:09 +0530

    dsa

--- src/LINKEDLIST/circularLinkedList/Q1.java
+++ src/LINKEDLIST/circularLinkedList/Q1.java
@@ -7,39 +7,33 @@ public class Q1 {
         int val;
         ListNode next;
 
-        ListNode(int val){
+        ListNode(int val) {
             this.val = val;
         }
     }
 
-    static ListNode deleteHead(ListNode head){
+    static ListNode deleteHead(ListNode head) {
 
 
-        if(head==null || head.next==head) return null;
+        if (head == null || head.next == head) return null;
 
-        ListNode temp=head;
+        head.val = head.next.val;
 
-        while (temp.next!=head){
-            temp=temp.next;
-        }
+        head.next = head.next.next;
 
-        temp.next=head.next;
-
-        return head.next;
-
+        return head;
 
 
     }
 
-    static void display(ListNode head){
+    static void display(ListNode head) {
 
-        ListNode temp=head.next;
-        System.out.print(head.val +" ");
-        while (temp!=head){
-            System.out.print(temp.val +" ");
-            temp=temp.next;
+        ListNode temp = head.next;
+        System.out.print(head.val + " ");
+        while (temp != head) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
         }
-
 
 
         System.out.println();
@@ -50,25 +44,21 @@ public class Q1 {
         // only head is given of circular linked list . Delete the Head
 
 
-        ListNode a=new ListNode(1);
-        ListNode b=new ListNode(5);
-        ListNode c=new ListNode(4);
-        ListNode d=new ListNode(3);
-        ListNode e=new ListNode(7);
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(5);
+        ListNode c = new ListNode(4);
+        ListNode d = new ListNode(3);
+        ListNode e = new ListNode(7);
 
-        a.next=b;
-        b.next=c;
-        c.next=d;
-        d.next=e;
-        e.next=a;
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = a;
 
         display(a);
-        ListNode Ans=deleteHead(a);
+        ListNode Ans = deleteHead(a);
         display(Ans);
-
-
-
-
 
 
     }
