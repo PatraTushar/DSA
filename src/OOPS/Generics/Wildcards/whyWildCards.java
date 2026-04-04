dcfa5ea9a8a9cc406b85e8329740e7732379c17b
Tushar Patra <tushar04012002@gmail.com>
2026-04-04 18:40:34 +0530

    dsa

--- src/OOPS/Generics/Wildcards/whyWildCards.java
+++ src/OOPS/Generics/Wildcards/whyWildCards.java
@@ -5,9 +5,9 @@ import java.util.Objects;
 
 public class whyWildCards {
 
-    public static void printList(List<Objects> list){
+    public static void printList(List<Objects> list) {
 
-        for(Object ele:list){
+        for (Object ele : list) {
             System.out.println(ele);
         }
     }
@@ -15,8 +15,12 @@ public class whyWildCards {
     public static void main(String[] args) {
 
 
-        List<String> obj=List.of("a","b");
-      //  printList(obj);     // compile-time error(Even though String is a subclass of Object, List<String> is not a subclass of List<Object>.)
+        List<String> obj = List.of("a", "b");
+        //  printList(obj);     // compile-time error(Even though String is a subclass of Object, List<String> is not a subclass of List<Object>.)
 
     }
 }
+
+
+// “In Java generics, a wildcard ? represents an unknown type. Wildcards are used to make code more flexible
+// and type-safe, allowing a method or class to work with different generic types without knowing the exact type in advance.”
