2b8b430063e7292505be2d15d7e937d5757e323f
Tushar Patra <tushar04012002@gmail.com>
2026-04-04 14:09:33 +0530

    dsa

--- src/OOPS/Generics/GenericMethod/T_extends_Type/MultipleInterfaceBound.java
+++ src/OOPS/Generics/GenericMethod/T_extends_Type/MultipleInterfaceBound.java
@@ -10,36 +10,39 @@ interface Showable {
     void show();
 }
 
+
+class Documents implements Printable, Showable {
+
+    public void print() {
+
+        System.out.println("Printing Document");
+    }
+
+    public void show() {
+
+        System.out.println("Showing Document");
+    }
+}
+
+
+class Demo {
+
+    public <T extends Printable & Showable> void process(T obj) {
+
+        obj.print();
+        obj.show();
+
+
+    }
+}
+
 public class MultipleInterfaceBound {
 
-    public static class Demo {
-
-        public <T extends Printable & Showable> void process(T obj) {
-
-            obj.print();
-            obj.show();
-
-
-        }
-    }
 
     public static void main(String[] args) {
 
-        class Documents implements Printable,Showable{
 
-            public void print(){
-
-                System.out.println("Printing Document");
-            }
-
-            public void show(){
-
-                System.out.println("Showing Document");
-            }
-        }
-
-
-        Demo d=new Demo();
+        Demo d = new Demo();
         d.process(new Documents());
 
 
