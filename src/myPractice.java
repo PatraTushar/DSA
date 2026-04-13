a2006de44ce145ecb341219cf9e81f36b5c613d7
Tushar Patra <tushar04012002@gmail.com>
2026-04-13 23:21:55 +0530

    dsa

--- src/myPractice.java
+++ src/myPractice.java
@@ -1,25 +1,25 @@
-import java.util.Arrays;
-import java.util.Stack;
-
-public class myPractice {
-
-    static int largestRectangleHistogram(int[] heights) {
-
-
-
-    }
-
-    public static void main(String[] args) {
-
-        int[] arr = {2, 1, 5, 6, 2, 3};
-        System.out.println(largestRectangleHistogram(arr));
-
-    }
-
-
-}
-
-
-
-
-
+//import java.util.Arrays;
+//import java.util.Stack;
+//
+//public class myPractice {
+//
+//    static int largestRectangleHistogram(int[] heights) {
+//
+//
+//
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        int[] arr = {2, 1, 5, 6, 2, 3};
+//        System.out.println(largestRectangleHistogram(arr));
+//
+//    }
+//
+//
+//}
+//
+//
+//
+//
+//
