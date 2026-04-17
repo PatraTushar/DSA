7a88efc4e5c2db00cca6ff341801ec8a49e864bc
Tushar Patra <tushar04012002@gmail.com>
2026-04-18 01:42:13 +0530

    dsa

--- src/STACKS/interviewQuestions/Q11.java
+++ src/STACKS/interviewQuestions/Q11.java
@@ -54,7 +54,7 @@ public class Q11 {
 
     public static void main(String[] args) {
 
-        int[] arr = {1, 4, 3, 2};
+        int[] arr = {3,1,2,4};
         System.out.println(sumOfSubArrMax(arr));
 
 
--- src/myPractice.java
+++ src/myPractice.java
@@ -111,10 +111,137 @@ public class myPractice {
     }
 
 
+    static int sumOfSubArrayMinimums(int[] arr) {
 
+
+        int length = arr.length;
+        int MOD = (int) 1e9 + 7;
+        int[] pse = new int[length];
+        int[] nse = new int[length];
+        Stack<Integer> st = new Stack<>();
+
+
+        for (int i = length - 1; i >= 0; i--) {
+
+            while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
+
+                st.pop();
+            }
+
+
+            nse[i] = st.isEmpty() ? length : st.peek();
+
+            st.push(i);
+
+
+        }
+
+        st.clear();
+
+
+        for (int i = 0; i < length; i++) {
+
+            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+
+                st.pop();
+            }
+
+
+            pse[i] = st.isEmpty() ? -1 : st.peek();
+
+            st.push(i);
+
+
+        }
+
+
+        long res = 0;
+
+        for (int i = 0; i < length; i++) {
+
+            long left = i - pse[i];
+            long right = nse[i] - i;
+
+            res = (res + left * right * arr[i]) % MOD;
+        }
+
+        return (int) res;
+
+
+    }
+
+
+    static int sumOfSubArrayMaximums(int[] arr) {
+
+
+        int length = arr.length;
+        int MOD = (int) 1e9 + 7;
+        int[] pge = new int[length];
+        int[] nge = new int[length];
+        Stack<Integer> st = new Stack<>();
+
+
+        for (int i = 0; i < length; i++) {
+
+            while (!st.isEmpty() && arr[st.peek()] < arr[i]) {
+
+                st.pop();
+            }
+
+
+            pge[i] = st.isEmpty() ? -1 : st.peek();
+
+            st.push(i);
+
+
+        }
+
+        st.clear();
+
+
+        for (int i = length - 1; i >= 0; i--) {
+
+            while (!st.isEmpty() && arr[st.peek()] <= arr[i]) {
+
+                st.pop();
+            }
+
+
+            nge[i] = st.isEmpty() ? length : st.peek();
+
+            st.push(i);
+
+
+        }
+
+
+
+
+
+
+
+
+        long res = 0;
+
+        for (int i = 0; i < length; i++) {
+
+            long left = i - pge[i];
+            long right = nge[i] - i;
+
+            res = (res + left * right * arr[i]) % MOD;
+        }
+
+        return (int) res;
+
+
+    }
 
 
     public static void main(String[] args) {
+
+        int[] num = {3, 1, 2, 4};
+         System.out.println(sumOfSubArrayMinimums(num));
+        System.out.println(sumOfSubArrayMaximums(num));
 
 
     }
