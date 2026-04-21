3982735620996fd07dcd84d7f6aa529b70fd90ea
Tushar Patra <tushar04012002@gmail.com>
2026-04-21 18:37:37 +0530

    dsa

--- src/MultithreadingAndConcurrency/preventingThreadFromExecution/myThreadYieldDemo.java
+++ src/MultithreadingAndConcurrency/preventingThreadFromExecution/myThreadYieldDemo.java
@@ -19,6 +19,12 @@ public class myThreadYieldDemo {
     public static void main(String[] args) {
 
         myThread obj = new myThread();
+
+        // Child thread gets lower priority
+        obj.setPriority(Thread.MIN_PRIORITY);
+
+        // Main thread gets higher priority
+        Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
         obj.start();
 
 
@@ -29,3 +35,16 @@ public class myThreadYieldDemo {
 
     }
 }
+
+
+//Thread.yield() is a static method that suggests to the thread scheduler that the currently executing thread is willing to
+// temporarily pause its execution, allowing other threads of same or higher priority to execute.
+// However, this is only a hint, and the scheduler is not guaranteed to honor it, so its behavior is platform and JVM dependent.
+
+//👉 The thread scheduler may accept or ignore Thread.yield().
+//
+//So:
+//
+//✔ It can accept it → switch to another thread
+//✔ It can ignore it → continue running the same thread
+//✔ It is completely JVM + OS dependent
