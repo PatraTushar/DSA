1fbed76e046f5e41899228c915da38ddfbff9566
Tushar Patra <tushar04012002@gmail.com>
2026-04-24 00:32:19 +0530

    dsa

---
+++ src/STRINGS/InterviewQuestions/Q4.java
@@ -0,0 +1,40 @@
+package STRINGS.InterviewQuestions;
+
+public class Q4 {
+
+    static String intToRoman(int num) {
+
+        // Time Complexity: O(1)
+        // Space Complexity: O(1)
+
+
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder sb = new StringBuilder();
+
+
+        for (int i = 0; i < values.length; i++) {
+
+            while (num >= values[i]) {
+
+                num -= values[i];
+                sb.append(symbols[i]);
+
+            }
+        }
+
+        return sb.toString();
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        // leeTCode->12
+
+        int num = 3749;
+        System.out.println(intToRoman(num));
+    }
+}
