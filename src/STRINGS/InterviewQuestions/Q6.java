3eec4a90ea6770d55059d70b1f39359ef18598e3
Tushar Patra <tushar04012002@gmail.com>
2026-04-24 14:21:59 +0530

    dsa

---
+++ src/STRINGS/InterviewQuestions/Q6.java
@@ -0,0 +1,51 @@
+package STRINGS.InterviewQuestions;
+
+import java.util.Arrays;
+
+public class Q6 {
+
+    // Time Complexity:   k == 1 → O(n²)    k > 1 → O(n log n)
+    // Space Complexity: O(n)
+
+    static String orderlyQueue(String s, int k) {
+
+        String result = s;
+
+        if (k == 1) {
+
+
+
+            for (int i = 1; i < s.length(); i++) {
+
+                String rotated = s.substring(i) + s.substring(0, i);
+
+                if (rotated.compareTo(result) < 0) {
+
+                    result=rotated;
+                }
+
+            }
+
+
+        } else {
+
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            return new String(chars);
+        }
+
+        return result;
+
+
+    }
+
+    public static void main(String[] args) {
+
+        // leeTCode->
+
+        String s = "baaca";
+        int k = 3;
+        String ans = orderlyQueue(s, k);
+        System.out.println(ans);
+    }
+}
