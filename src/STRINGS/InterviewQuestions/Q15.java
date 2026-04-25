499ed4a1ba2f04c52b07ecf3d318f529382b7044
Tushar Patra <tushar04012002@gmail.com>
2026-04-26 02:14:14 +0530

    dsa

---
+++ src/STRINGS/InterviewQuestions/Q15.java
@@ -0,0 +1,20 @@
+//package STRINGS.InterviewQuestions;
+//
+//public class Q15 {
+//
+//    static boolean buddyStrings(String s, String goal) {
+//
+//
+//
+//
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        //leeTCode->859
+//
+//        String s = "ab";
+//        String goal = "ba";
+//        System.out.println(buddyStrings(s, goal));
+//    }
+//}
