b7c2a4a489d01d269cce2bc1c6a83a9bf49528a9
Tushar Patra <tushar04012002@gmail.com>
2026-04-27 17:13:30 +0530

    dsa

---
+++ src/STRINGS/InterviewQuestions/Q16.java
@@ -0,0 +1,71 @@
+package STRINGS.InterviewQuestions;
+
+public class Q16 {
+
+    static int largestVariance(String s) {
+
+        // Time Complexity: O(26 × 26 × n) ≈ O(n)
+        // Space Complexity: O(26) ≈ O(1)**
+
+
+        int[] freq = new int[26];
+
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        int result = 0;
+
+        for (char first = 'a'; first <= 'z'; first++) {
+
+            for (char second = 'a'; second <= 'z'; second++) {
+
+                if (freq[first - 'a'] == 0 || freq[second - 'a'] == 0) continue;
+
+                int firstCount=0;
+                int secondCount=0;
+
+                boolean pastSecond=false;
+
+                for (char ch:s.toCharArray()){
+
+                    if (ch==first) firstCount++;
+
+                     if (ch==second) secondCount++;
+
+                    if (secondCount>0) result=Math.max(result,firstCount-secondCount);
+
+                    else {
+
+                        if (pastSecond){
+
+                            result=Math.max(result,firstCount-1);
+                        }
+                    }
+
+                    if (secondCount>firstCount){
+                        secondCount=0;
+                        firstCount=0;
+
+                        pastSecond=true;
+                    }
+                }
+
+
+            }
+        }
+
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+
+        //leeTCode->2272
+
+        String s = "aababbb";
+        System.out.println(largestVariance(s));
+
+
+    }
+}
