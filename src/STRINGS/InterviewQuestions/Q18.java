e3d20c12f291d063dbe40cdcfb26fedb1cdadd1a
Tushar Patra <tushar04012002@gmail.com>
2026-04-27 20:54:33 +0530

    dsa

---
+++ src/STRINGS/InterviewQuestions/Q18.java
@@ -0,0 +1,31 @@
+//package STRINGS.InterviewQuestions;
+//
+//import java.util.List;
+//
+//public class Q18 {
+//
+//    static List<String> fullJustify(String[] words,int maxWidth){
+//
+//
+//        // Time Complexity:  O(n)
+//        // Space Complexity: O(n)
+//
+//
+//
+//
+//
+//
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        // leeTCode->68
+//
+//        String[] words={"This", "is", "an", "example", "of", "text", "justification."};
+//        int maxWidth=16;
+//        List<String> ans=fullJustify(words,maxWidth);
+//        System.out.println(ans);
+//
+//
+//    }
+//}
