2c6c2635ed8308dde8ed841483e8cf973f841529
Tushar Patra <tushar04012002@gmail.com>
2026-04-27 13:26:17 +0530

    dsa

--- src/STRINGS/InterviewQuestions/Q15.java
+++ src/STRINGS/InterviewQuestions/Q15.java
@@ -1,20 +1,59 @@
-//package STRINGS.InterviewQuestions;
-//
-//public class Q15 {
-//
-//    static boolean buddyStrings(String s, String goal) {
-//
-//
-//
-//
-//    }
-//
-//    public static void main(String[] args) {
-//
-//        //leeTCode->859
-//
-//        String s = "ab";
-//        String goal = "ba";
-//        System.out.println(buddyStrings(s, goal));
-//    }
-//}
+package STRINGS.InterviewQuestions;
+
+public class Q15 {
+
+    static boolean buddyStrings(String s, String goal) {
+
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+
+        int n = s.length();
+
+        if (s.length() != goal.length()) return false;
+
+        if (s.equals(goal)) {
+            int[] freq = new int[26];
+
+            for (int i = 0; i < n; i++) {
+
+                char ch = s.charAt(i);
+
+                freq[ch - 'a']++;
+
+                if (freq[ch - 'a'] > 1) return true;
+
+
+            }
+        }
+
+
+        int index1 = -1;
+        int index2 = -1;
+
+
+        for (int i = 0; i < n; i++) {
+
+            if (s.charAt(i)!=goal.charAt(i)){
+
+                if (index1==-1) index1=i;
+                else if (index2==-1)index2=i;
+                else return false;
+            }
+        }
+
+        if (index2==-1) return false;
+
+        return s.charAt(index1)==goal.charAt(index2) &&s.charAt(index2)==goal.charAt(index1);
+
+
+    }
+
+    public static void main(String[] args) {
+
+        //leeTCode->859
+
+        String s = "ab";
+        String goal = "ba";
+        System.out.println(buddyStrings(s, goal));
+    }
+}
