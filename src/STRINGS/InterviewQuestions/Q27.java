27199eecba7c0ed02af58e93f455ca8affefe02b
Tushar Patra <tushar04012002@gmail.com>
2026-04-30 11:10:05 +0530

    dsa

--- src/STRINGS/InterviewQuestions/Q27.java
+++ src/STRINGS/InterviewQuestions/Q27.java
@@ -1,20 +1,61 @@
-//package STRINGS.InterviewQuestions;
-//
-//public class Q27 {
-//
-//    static int beautifulSubstrings(String s, int k) {
-//
-//
-//
-//    }
-//
-//
-//    public static void main(String[] args) {
-//
-//        // leeTCode->2947
-//
-//        String s="baeyh";
-//        int k=2;
-//        System.out.println(beautifulSubstrings(s,k));
-//    }
-//}
+package STRINGS.InterviewQuestions;
+
+public class Q27 {
+
+    static boolean isVowel(char ch) {
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') return true;
+
+        return false;
+    }
+
+    static int beautifulSubstrings(String s, int k) {
+
+        // Time Complexity: O(n²)
+        // Space Complexity: O(1)
+
+        int n = s.length();
+        int count = 0;
+
+
+        for (int i = 0; i < n; i++) {
+
+            int vowels = 0;
+            int consonants = 0;
+
+            for (int j = i; j < n; j++) {
+
+                char ch = s.charAt(j);
+
+                if (isVowel(ch)) vowels++;
+
+                else consonants++;
+
+                if (vowels == consonants) {
+
+                    int x = vowels;
+
+                    if ((x * x) % k == 0) count++;
+
+
+                }
+            }
+
+
+        }
+
+        return count;
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        // leeTCode->2947
+
+        String s = "baeyh";
+        int k = 2;
+        System.out.println(beautifulSubstrings(s, k));
+    }
+}
