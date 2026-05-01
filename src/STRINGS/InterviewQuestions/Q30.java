95fcecb45ab822a6aa9e842dd5c6eb0ef42399a2
Tushar Patra <tushar04012002@gmail.com>
2026-05-01 11:46:35 +0530

    dsa

---
+++ src/STRINGS/InterviewQuestions/Q30.java
@@ -0,0 +1,41 @@
+package STRINGS.InterviewQuestions;
+
+public class Q30 {
+
+
+    static int numberOfBeams(String[] bank) {
+
+        // Time Complexity: O(m*n)
+        // Space Complexity: O(1)
+
+        int n = bank.length;
+
+        int prevDeviceCount = 0;
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            int currDeviceCount = 0;
+
+            for (char ch : bank[i].toCharArray()) {
+
+                if (ch == '1') currDeviceCount++;
+
+            }
+
+            result += (prevDeviceCount * currDeviceCount);
+
+            prevDeviceCount = (currDeviceCount == 0) ? prevDeviceCount : currDeviceCount;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        String[] bank = {"011001", "000000", "010100", "001000"};
+        System.out.println(numberOfBeams(bank));
+
+
+    }
+}
