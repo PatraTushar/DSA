b761833908ca0264bc6c2a9f30e9002b4cad2a22
Tushar Patra <tushar04012002@gmail.com>
2026-05-02 22:20:14 +0530

    dsa

--- src/Java8Features/FunctionalInterface/BiConsumerDemo.java
+++ src/Java8Features/FunctionalInterface/BiConsumerDemo.java
@@ -1,10 +1,6 @@
 package Java8Features.FunctionalInterface;
 
-
-import OOPS.AccessModifier.Public.A;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.function.BiConsumer;
 
 class MyEmployee {
@@ -50,7 +46,7 @@ public class BiConsumerDemo {
 
         for (MyEmployee e : list) {
 
-            System.out.println(e.name+":"+e.salary);
+            System.out.println(e.name + ":" + e.salary);
         }
 
 
