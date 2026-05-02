4e6a82d96e63861fbd341e083bd0955b6626e9d0
Tushar Patra <tushar04012002@gmail.com>
2026-05-02 22:18:25 +0530

    dsa

---
+++ src/Java8Features/FunctionalInterface/BiConsumerDemo.java
@@ -0,0 +1,58 @@
+package Java8Features.FunctionalInterface;
+
+
+import OOPS.AccessModifier.Public.A;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.BiConsumer;
+
+class MyEmployee {
+
+    String name;
+    double salary;
+
+    MyEmployee(String name, double salary) {
+
+        this.name = name;
+        this.salary = salary;
+    }
+
+
+}
+
+
+public class BiConsumerDemo {
+
+    static void populate(ArrayList<MyEmployee> l) {
+
+        l.add(new MyEmployee("Rahul", 1000));
+        l.add(new MyEmployee("Mehul", 2000));
+        l.add(new MyEmployee("Virat", 3000));
+        l.add(new MyEmployee("Hardik", 4000));
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        ArrayList<MyEmployee> list = new ArrayList<>();
+        populate(list);
+
+        BiConsumer<MyEmployee, Double> b = (e, d) -> e.salary += d;
+
+        for (MyEmployee e : list) {
+
+            b.accept(e, 500.00);
+        }
+
+
+        for (MyEmployee e : list) {
+
+            System.out.println(e.name+":"+e.salary);
+        }
+
+
+    }
+}
--- src/Java8Features/FunctionalInterface/BiFunctionDemo.java
+++ src/Java8Features/FunctionalInterface/BiFunctionDemo.java
@@ -1,8 +1,42 @@
 package Java8Features.FunctionalInterface;
 
+import java.util.ArrayList;
+import java.util.function.BiFunction;
+
+
+class Employee {
+
+    int eno;
+    String name;
+
+    Employee(int eno, String name) {
+
+        this.eno = eno;
+        this.name = name;
+    }
+
+
+
+
+}
+
 public class BiFunctionDemo {
 
     public static void main(String[] args) {
+
+        ArrayList<Employee> list = new ArrayList<>();
+
+        BiFunction<Integer, String, Employee> a = (eno, name) -> new Employee(eno, name);
+        list.add(a.apply(100, "Rahul"));
+        list.add(a.apply(200, "Aman"));
+        list.add(a.apply(300, "Bipul"));
+        list.add(a.apply(400, "Ehan"));
+
+        for (Employee e : list) {
+
+            System.out.println(e.eno + ":" + e.name);
+
+        }
 
     }
 }
