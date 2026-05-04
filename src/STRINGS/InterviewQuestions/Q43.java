89acae746ed7328950c8b54e94203729540735c7
Tushar Patra <tushar04012002@gmail.com>
2026-05-04 17:15:57 +0530

    dsa

--- src/STRINGS/InterviewQuestions/Q43.java
+++ src/STRINGS/InterviewQuestions/Q43.java
@@ -28,13 +28,15 @@ public class Q43 {
         int i = 0, j = vec1.size() - 1;
         int k = 0, l = vec2.size() - 1;
 
-        while (i < vec1.size() && k < vec2.size() && vec1.get(i).equals(vec2.get(k))) {
+        // Check for matching prefix
+        while (k < vec2.size() && i < vec1.size() && vec2.get(k).equals(vec1.get(i))) {
 
             i++;
             k++;
         }
 
-        while (j >= i && l >= k && vec1.get(j).equals(vec2.get(l))) {
+        // Check for matching suffix
+        while (  l >= k && j >= i && vec2.get(l).equals(vec1.get(j))) {
 
             j--;
             l--;
