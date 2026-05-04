4c36b20fcfd4be9ee4912b2ce43d0a702ea65c65
Tushar Patra <tushar04012002@gmail.com>
2026-05-05 00:12:18 +0530

    dsa

--- src/Java8Features/streams.java
+++ src/Java8Features/streams.java
@@ -3,6 +3,7 @@ package Java8Features;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class streams {
 
@@ -38,6 +39,72 @@ public class streams {
 
         List<Integer> Desc = marks.stream().sorted((a, b) -> (a > b) ? -1 : (a < b) ? 1 : 0).collect(Collectors.toList());
         System.out.println(Desc);
+
+        List<String> names = new ArrayList<>();
+        names.add("Rohit Sharma");
+        names.add("Devdutt Padikkal");
+        names.add("MS Dhoni");
+        names.add("Virat Kohli");
+        names.add("Abhishek Sharma");
+
+
+        // sort names in ascending order
+        List<String> res = names.stream().sorted().collect(Collectors.toList());
+        System.out.println(res);
+
+        // sort names according to length wise
+        List<String> res1 = names.stream().sorted((s1, s2) -> {
+            int l1 = s1.length();
+            int l2 = s2.length();
+            if (l1 < l2) return -1;
+            else if (l1 > l2) return +1;
+            else return s1.compareTo(s2);
+        }).collect(Collectors.toList());
+        System.out.println(res1);
+
+
+
+        List<Integer> number=new ArrayList<>();
+        number.add(10);
+        number.add(1);
+        number.add(5);
+        number.add(15);
+        number.add(20);
+
+        System.out.println(number);
+
+        Integer min=number.stream().min((a,b)->a.compareTo(b)).get();
+        System.out.println(min);
+
+        Integer max=number.stream().max((a,b)->a.compareTo(b)).get();
+        System.out.println(max);
+
+
+        // print element from the list
+
+        List<Integer> num=new ArrayList<>();
+        num.add(50);
+        num.add(100);
+        num.add(150);
+        num.add(200);
+        num.add(250);
+
+        num.stream().forEach(System.out::println);
+
+
+
+        List<Double> d=new ArrayList<>();
+        d.add(19.0);
+        d.add(21.1);
+        d.add(13.5);
+        d.add(112.45);
+        d.add(53.76);
+
+        Double[] ans=d.stream().toArray(Double[]::new);
+        Stream.of(ans).forEach(System.out::println);
+
+
+
 
 
     }
