5e0ad4ec5cb00c7a21ef31a3d588854217f03c83
Tushar Patra <tushar04012002@gmail.com>
2026-05-05 22:17:48 +0530

    dsa

--- src/STRINGS/InterviewQuestions/Q47.java
+++ src/STRINGS/InterviewQuestions/Q47.java
@@ -1,4 +1,46 @@
 package STRINGS.InterviewQuestions;
 
 public class Q47 {
+
+    static String compressedString(String word) {
+
+
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+
+        int n = word.length();
+        StringBuilder comp = new StringBuilder();
+
+        int i = 0;
+
+        while (i < n) {
+
+            char ch = word.charAt(i);
+            int count = 0;
+
+            while (i < n && count < 9 && word.charAt(i) == ch) {
+
+                count++;
+                i++;
+            }
+
+            comp.append(count).append(ch);
+
+
+        }
+
+        return comp.toString();
+
+
+    }
+
+    public static void main(String[] args) {
+
+        // leeTCode->3163
+
+        String word = "abcde";
+        System.out.println(compressedString(word));
+
+
+    }
 }
