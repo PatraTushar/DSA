260f993cd0b9e898754861d190a7bd8c5fc7f431
Tushar Patra <tushar04012002@gmail.com>
2026-05-05 20:32:31 +0530

    dsa

--- src/STRINGS/InterviewQuestions/Q45.java
+++ src/STRINGS/InterviewQuestions/Q45.java
@@ -1,19 +1,65 @@
-//package STRINGS.InterviewQuestions;
-//
-//public class Q45 {
-//
-//    static int maximumSwap(int num){
-//
-//
-//
-//
-//    }
-//
-//    public static void main(String[] args) {
-//
-//        // leeTCode->670
-//
-//        int num=2736;
-//        System.out.println(maximumSwap(num));
-//    }
-//}
+package STRINGS.InterviewQuestions;
+
+import java.util.Arrays;
+
+public class Q45 {
+
+    static int maximumSwap(int num) {
+
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+
+        String s = Integer.toString(num);
+        int n = s.length();
+
+        int[] maxRight = new int[10];
+
+        Arrays.fill(maxRight, -1);
+
+        for (int i = 0; i < n; i++) {
+
+            int idx = s.charAt(i) - '0';
+
+            maxRight[idx] = i;
+        }
+
+
+        for (int i = 0; i < n; i++) {
+
+            int currDigit = s.charAt(i) - '0';
+
+            for (int digit = 9; digit > currDigit; digit--) {
+
+                if (maxRight[digit] > i) {
+
+                    char[] charArray=s.toCharArray();
+                    char temp=charArray[i];
+                    charArray[i]=charArray[maxRight[digit]];
+                    charArray[maxRight[digit]]=temp;
+
+                    return Integer.parseInt(new String(charArray));
+
+
+
+                }
+
+
+
+
+
+            }
+        }
+
+        return num;
+
+
+    }
+
+    public static void main(String[] args) {
+
+        // leeTCode->670
+
+        int num = 2736;
+        System.out.println(maximumSwap(num));
+    }
+}
--- src/myPractice.java
+++ src/myPractice.java
@@ -1,29 +1,12 @@
 
 
-
 public class myPractice {
-
-    static void m1() {
-
-        for (int i = 0; i < 10; i++) {
-
-            System.out.println(" child thread ");
-        }
-    }
 
     public static void main(String[] args) {
 
-        Runnable r = myPractice::m1;
 
-        Thread t=new Thread(r);
-
-        for (int i=0;i<10;i++){
-
-            System.out.println(" main thread ");
-        }
 
     }
-
 }
 
 
