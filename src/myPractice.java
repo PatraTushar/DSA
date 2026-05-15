488a64c14c97a2f31c47c7e8ec79c246c9407e3b
Tushar Patra <tushar04012002@gmail.com>
2026-05-15 23:50:39 +0530

    dsa

--- src/myPractice.java
+++ src/myPractice.java
@@ -1,23 +1,8 @@
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class myPractice {
 
-
     public static void main(String[] args) {
-
-        List<String> list = new ArrayList<>();
-        list.add("A");
-        list.add("B");
-        list.add("C");
-        list.add("D");
-
-        for (String s : list) {
-
-            if (s.equals("B"))list.remove(s);
-        }
-
-        System.out.println(list);
 
 
     }
