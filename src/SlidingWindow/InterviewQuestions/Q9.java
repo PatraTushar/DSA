edc3d2cc044cdfe250f31d2c9655ad261e9b9175
Tushar Patra <tushar04012002@gmail.com>
2026-05-22 09:30:18 +0530

    dsa

--- src/SlidingWindow/InterviewQuestions/Q9.java
+++ src/SlidingWindow/InterviewQuestions/Q9.java
@@ -2,15 +2,89 @@ package SlidingWindow.InterviewQuestions;
 
 public class Q9 {
 
-    static int longestSubArray(int[] nums){
+    static int findMax(int[] num, int skipIndex) {
 
-        int n=nums.length;
+        int n = num.length;
+        int currLength = 0;
+        int maxLength = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (i == skipIndex) continue;
+
+
+            if (num[i] == 1) {
+
+                currLength++;
+                maxLength = Math.max(currLength, maxLength);
+            } else currLength = 0;
+
+
+        }
+
+        return maxLength;
+    }
+
+    static int longestSubArray(int[] num) {
+
+        // Time Complexity: O(n²)
+        // Space Complexity: O(1)
+
+        int n = num.length;
+        int result = 0;
+        int countZero = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (num[i] == 0) {
+
+                countZero++;
+                result = Math.max(result, findMax(num, i));
+
+            }
+        }
+
+        if (countZero == 0) return n - 1;
+
+        return result;
+    }
+
+    static int longestSubArrayI(int[] nums) {
+
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+
+        int n = nums.length;
+        int zeroCount = 0;
+        int result = 0;
+
+        int left = 0;
+
+        for (int right = 0; right < n; right++) {
+
+            if (nums[right] == 0) zeroCount++;
+
+            while (zeroCount > 1) {
+
+                if (nums[left] == 0) zeroCount--;
+                left++;
+
+
+            }
+
+            result = Math.max(result, right - left);
+        }
+
+        return result;
+
+
     }
 
     public static void main(String[] args) {
 
-        int[] num={1,1,0,1};
+        int[] num = {1, 1, 0, 1};
         System.out.println(longestSubArray(num));
+        System.out.println(longestSubArrayI(num));
 
 
     }
--- src/myPractice.java
+++ src/myPractice.java
@@ -1,8 +1,29 @@
-
+import java.util.*;
 
 public class myPractice {
 
+
     public static void main(String[] args) {
+
+        List<Integer> list = new Vector<>();
+        list.add(100);
+        list.add(2001);
+        list.add(3001);
+        list.add(4001);
+        list.add(500);
+
+        ListIterator<Integer> e = list.listIterator();
+
+        while (e.hasNext()) {
+
+            int ele=e.next();
+
+            if (ele%2==0) e.set(404040404);
+
+        }
+
+
+        System.out.println(list);
 
 
     }
