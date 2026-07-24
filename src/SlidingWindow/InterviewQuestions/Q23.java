628581d9af2b6666e83303e29c594c7b706f1e0d
Tushar Patra <tushar04012002@gmail.com>
2026-07-24 19:46:29 +0530

    dsa

--- src/SlidingWindow/InterviewQuestions/Q23.java
+++ src/SlidingWindow/InterviewQuestions/Q23.java
@@ -1,22 +1,22 @@
-package SlidingWindow.InterviewQuestions;
-
-public class Q23 {
-
-    static int shortestSubArray(int[] nums,int k){
-
-        int n=nums.length;
-
-
-    }
-
-    public static void main(String[] args) {
-
-        // leeTCode->862
-
-        int[] num={2,-1,2};
-        int k=3;
-        System.out.println(shortestSubArray(num,k));
-
-
-    }
-}
+//package SlidingWindow.InterviewQuestions;
+//
+//public class Q23 {
+//
+//    static int shortestSubArray(int[] nums,int k){
+//
+//        int n=nums.length;
+//
+//
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        // leeTCode->862
+//
+//        int[] num={2,-1,2};
+//        int k=3;
+//        System.out.println(shortestSubArray(num,k));
+//
+//
+//    }
+//}
