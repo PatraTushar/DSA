bd2f4016dbd2049229bc4f94352624bf5c43a8e8
Tushar Patra <tushar04012002@gmail.com>
2026-07-27 14:10:36 +0530

    dsa

--- src/SlidingWindow/InterviewQuestions/Q25.java
+++ src/SlidingWindow/InterviewQuestions/Q25.java
@@ -2,7 +2,63 @@ package SlidingWindow.InterviewQuestions;
 
 public class Q25 {
 
+    static int takeCharacters(String s, int k) {
+
+        // Time Complexity (TC): O(n)
+        //Space Complexity (SC): O(1)
+
+
+        int n = s.length();
+
+        int a = 0, b = 0, c = 0;
+
+        for (Character ch : s.toCharArray()) {
+
+            if (ch == 'a') a++;
+            else if (ch == 'b') b++;
+            else c++;
+        }
+
+
+        if (a < k || b < k || c < k) return -1;
+
+        int left = 0;
+        int notDeletedWindowSize = 0;
+
+        for (int right = 0; right < n; right++) {
+
+            char ch = s.charAt(right);
+
+            if (ch == 'a') a--;
+            else if (ch == 'b') b--;
+            else c--;
+
+
+            while (a < k || b < k || c < k) {
+
+                if (s.charAt(left) == 'a') a++;
+                else if (s.charAt(left) == 'b') b++;
+                else c++;
+
+                left++;
+            }
+
+
+            notDeletedWindowSize=Math.max(notDeletedWindowSize,right-left+1);
+        }
+
+        return n-notDeletedWindowSize;
+
+
+    }
+
     public static void main(String[] args) {
+
+        // leeTCode->2516
+
+        String s = "aabaaaacaabc";
+        int k = 2;
+        System.out.println(takeCharacters(s, k));
 
 
     }
---
+++ src/SlidingWindow/InterviewQuestions/Q26.java
@@ -0,0 +1,54 @@
+package SlidingWindow.InterviewQuestions;
+
+import java.util.TreeMap;
+
+public class Q26 {
+
+    static long continuousSubArrays(int[] num) {
+
+        // Time Complexity (TC): O(n)
+        //Space Complexity (SC): O(1)
+
+        int n = num.length;
+
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+
+        long result = 0;
+        int left = 0;
+
+        for (int right = 0; right < n; right++) {
+
+
+            // Expand the window
+            map.put(num[right], map.getOrDefault(num[right], 0) + 1);
+
+
+            // Window becomes invalid
+
+            while (map.lastKey() - map.firstKey() > 2) {
+
+                map.put(num[left], map.get(num[left])-1);
+
+                if (map.get(num[left])==0) map.remove(num[left]);
+
+                left++;
+            }
+
+            result+=right-left+1;
+
+
+        }
+
+        return result;
+
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] num = {5, 4, 2, 4};
+        System.out.println(continuousSubArrays(num));
+
+
+    }
+}
