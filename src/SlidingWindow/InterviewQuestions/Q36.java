e75ef6e19ae01d794fa3d02e6a8a0b1610afb275
Tushar Patra <tushar04012002@gmail.com>
2026-07-29 21:08:30 +0530

    dsa

---
+++ src/SlidingWindow/InterviewQuestions/Q36.java
@@ -0,0 +1,19 @@
+//package SlidingWindow.InterviewQuestions;
+//
+//public class Q36 {
+//
+//    static long minimumCost(int[] nums, int k, int dist) {
+//
+//        int n = nums.length;
+//
+//
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        int[] num = {1, 3, 2, 6, 4, 2};
+//        int k = 3;
+//        int dist = 3;
+//        System.out.println(minimumCost(num, k, dist));
+//    }
+//}
---
+++ src/SlidingWindow/InterviewQuestions/Q37.java
@@ -0,0 +1,20 @@
+package SlidingWindow.InterviewQuestions;
+
+public class Q37 {
+
+    static int minFlips(String s){
+
+        int n=s.length();
+
+
+    }
+
+    public static void main(String[] args) {
+
+        // leeTCode->1888
+
+        String s="111000";
+        System.out.println(minFlips(s));
+
+    }
+}
