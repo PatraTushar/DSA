781933261d38a7a84e31c0e064c147dd40f00973
Tushar Patra <tushar04012002@gmail.com>
2026-09-18 13:25:17 +0530

    dsa

---
+++ src/DSAPractice/Q66.java
@@ -0,0 +1,21 @@
+package DSAPractice;
+
+import java.util.Arrays;
+
+public class Q66 {
+
+    static int[][] merge(int[][] intervals) {
+
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        int[][] intervals = {{1, 3}, {2, 6}, {8, 9}, {9, 11}, {8, 10}, {2, 4}, {15, 18}, {16, 17}};
+        int[][] ans = merge(intervals);
+        System.out.println(Arrays.deepToString(ans));
+
+    }
+}
